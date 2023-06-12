3bcab8f4a470d160fb21803f022489bfcc98155c
Alexandre Léonardi <alexandre.leonardi@sekoia.io>
2023-06-12 14:10:36 +0000

    Add CrowdStrike_Falcon module

---
+++ CrowdStrikeFalcon/crowdstrike_falcon/action.py
@@ -0,0 +1,18 @@
+from functools import cached_property
+
+from sekoia_automation.action import Action
+
+from crowdstrike_falcon import CrowdStrikeFalconModule
+from crowdstrike_falcon.client import CrowdstrikeFalconClient
+
+
+class CrowdstrikeAction(Action):
+    module: CrowdStrikeFalconModule
+
+    @cached_property
+    def client(self):
+        return CrowdstrikeFalconClient(
+            self.module.configuration.base_url,
+            self.module.configuration.client_id,
+            self.module.configuration.client_secret,
+        )
---
+++ CrowdStrikeFalcon/crowdstrike_falcon/constants.py
@@ -0,0 +1,9 @@
+VERTICLES_TYPE_MAPPING = {
+    "mod": "modules",
+    "pid": "processes",
+    "dns": "domains",
+    "aid": "devices",
+    "uid": "users",
+    "uses": "user-sessions",
+    "ctg": "control-graphs",
+}
