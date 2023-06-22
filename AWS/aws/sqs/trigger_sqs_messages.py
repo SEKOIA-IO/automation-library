af867970afe01ef70ba18f244cf4f319f85e7026
Adamowoc <oceane.adamowski@sekoia.io>
2023-06-22 16:45:40 +0200

    move except

--- AWS/aws/sqs/trigger_sqs_messages.py
+++ AWS/aws/sqs/trigger_sqs_messages.py
@@ -35,36 +35,42 @@ class AWSSQSMessagesTrigger(AWSConnector):
         return self.sqs_client.get_queue_url(QueueName=self.queue_name)["QueueUrl"]
 
     def forward_next_batches(self, **kwargs):
-        response = self.sqs_client.receive_message(QueueUrl=self.queue_url, **kwargs)
+        response = self.sqs_client.receive_message(
+            QueueUrl=self.queue_url, **kwargs)
         messages = response.get("Messages", [])
 
         if len(messages) > 0:
             receipts = []
             records = []
             for message in messages:
+                decoded_messages = []
                 try:
-                    decoded_messages = orjson.loads(message["Body"].encode("utf-8")).get("Records", [])
-                    for record in decoded_messages:
-                        try:
-                            records.append(record)
-
-                            if receipt := message.get("ReceiptHandle"):
-                                receipts.append(receipt)
-                        except ValueError as e:
-                            self.log_exception(e, message="Invalid record")
+                    decoded_messages = orjson.loads(
+                        message["Body"].encode("utf-8")).get("Records", [])
                 except ValueError as e:
-                    self.log_exception(e, message=f"Invalid JSON in message.\nInvalid message is: {message}")
+                    self.log_exception(
+                        e, message=f"Invalid JSON in message.\nInvalid message is: {message}")
+                for record in decoded_messages:
+                    try:
+                        records.append(record)
 
+                        if receipt := message.get("ReceiptHandle"):
+                            receipts.append(receipt)
+                    except ValueError as e:
+                        self.log_exception(e, message="Invalid record")
             if records:
-                self.log(message=f"Forwarding {len(records)} messages", level="info")
+                self.log(
+                    message=f"Forwarding {len(records)} messages", level="info")
                 self.push_events_to_intakes(events=records)
             else:
                 self.log(message="No messages to forward", level="info")
 
             if self.delete_consumed_messages:
-                entries = [{"Id": f"{index:04d}", "ReceiptHandle": receipt} for index, receipt in enumerate(receipts)]
+                entries = [{"Id": f"{index:04d}", "ReceiptHandle": receipt}
+                           for index, receipt in enumerate(receipts)]
                 if entries:
-                    self.sqs_client.delete_message_batch(QueueUrl=self.queue_url, Entries=entries)
+                    self.sqs_client.delete_message_batch(
+                        QueueUrl=self.queue_url, Entries=entries)
         else:
             self.log(
                 message=f"No messages to process. Wait next batch in {self.configuration.frequency}s",
