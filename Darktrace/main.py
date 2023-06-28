42b2868da4dad1b518b40973cdac4e9a0f2eecfa
Adamowoc <oceane.adamowski@sekoia.io>
2023-06-28 17:13:27 +0200

    trigger in main

--- Darktrace/main.py
+++ Darktrace/main.py
@@ -1,5 +1,7 @@
 from darktrace_modules import DarktraceModule
+from darktrace_modules.threat_visualizer_log_trigger import ThreatVisualizerLogConnector
 
 if __name__ == "__main__":
     module = DarktraceModule()
+    module.register(ThreatVisualizerLogConnector, "github_audit_logs")
     module.run()
