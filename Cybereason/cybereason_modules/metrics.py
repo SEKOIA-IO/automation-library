ff193eb67859529e90315bf1ccf7a853b92b1a6a
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2023-07-14 12:42:34 +0200

    fix(Cybereason): add metrics

---
+++ Cybereason/cybereason_modules/metrics.py
@@ -0,0 +1,35 @@
+from prometheus_client import Counter, Histogram
+
+# Declare google prometheus metrics
+prom_namespace_cybereason = "symphony_module_cybereason"
+
+INCOMING_MALOPS = Counter(
+    name="collected_malops",
+    documentation="Number of malops collected from Cybereason",
+    namespace=prom_namespace_cybereason,
+    labelnames=["intake_key"],
+)
+
+# Declare common prometheus metrics
+prom_namespace = "symphony_module_common"
+
+OUTCOMING_EVENTS = Counter(
+    name="forwarded_events",
+    documentation="Number of events forwarded to SEKOIA.IO",
+    namespace=prom_namespace,
+    labelnames=["intake_key"],
+)
+
+EVENTS_LAG = Histogram(
+    name="events_lags",
+    documentation="The delay, in seconds, from the date of the last event",
+    namespace=prom_namespace,
+    labelnames=["intake_key"],
+)
+
+FORWARD_EVENTS_DURATION = Histogram(
+    name="events_forward_duration",
+    documentation="Duration to collect and forward events from  ybereason",
+    namespace=prom_namespace,
+    labelnames=["intake_key"],
+)
