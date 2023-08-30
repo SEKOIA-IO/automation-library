c00ed5b231bb810f4e91d5de201050ebaa73a995
Alexandre Léonardi <alexandre.leonardi@sekoia.io>
2023-08-30 14:30:18 +0000

    Add Retarus module
    It relies on websockets to fetch data and Kafka to forward them

---
+++ Retarus/retarus_modules/connector.py
@@ -0,0 +1,85 @@
+import os
+import queue
+import time
+from pathlib import Path
+from typing import Any, Optional
+
+from sekoia_automation.connector import Connector
+
+from retarus_modules.configuration import RetarusConfig
+from retarus_modules.consumer import RetarusEventsConsumer
+from retarus_modules.metrics import OUTGOING_EVENTS
+
+
+class RetarusConnector(Connector):
+    configuration: RetarusConfig
+
+    def __init__(self, *args: Any, **kwargs: Optional[Any]) -> None:
+        super().__init__(*args, **kwargs)
+        
+        # create the events queue
+        self.events_queue: queue.Queue = queue.Queue(maxsize=int(os.environ.get("QUEUE_SIZE", 10000)))
+
+        # Arguments for the polling of events from the queue
+        self.queue_get_limit = 10
+        self.queue_get_timeout = 0.1
+        self.queue_get_block = True
+        self.queue_get_retries = 10
+
+    def run(self):  # pragma: no cover
+        self.log(message="Retarus Events Trigger has started", level="info")
+
+        # start the consumer
+        consumer = RetarusEventsConsumer(self.configuration, self.events_queue, self.log, self.log_exception)
+        consumer.start()
+
+        while self.running:
+            # if the consumer is dead, we spawn a new one
+            if not consumer.is_alive() and consumer.is_running:
+                self.log(message="Restart event consumer", level="warning")
+                consumer = RetarusEventsConsumer(self.configuration, self.events_queue, self.log, self.log_exception)
+                consumer.start()
+
+            # Send events to Symphony
+            events = self._queue_get_batch()
+            if len(events) > 0:
+                self.log(
+                    message="Forward an event to the intake",
+                    level="info",
+                )
+                OUTGOING_EVENTS.labels(intake_key=self.configuration.intake_key).inc(len(events))
+                self.push_events_to_intakes(events=events)
+
+            # Wait 5 seconds for the next supervision
+            time.sleep(5)
+
+        # Stop the consumer
+        if consumer.is_alive():
+            consumer.stop()
+            consumer.join(timeout=2)
+
+        # Stop the connector executor
+        self._executor.shutdown(wait=True)
+
+    def _queue_get_batch(self) -> list[str]:
+        """Gets a batch of events from the queue
+
+        Several parameters for these batches can be set when initializing the class:
+        * queue_get_limit is the max number of messages we want to get for a match
+        * queue_get_retries is the max number of retries (empty queue exception) we accept for a given batch
+        * queue_get_block is the block parameter of queue.get
+        * queue_get_timeout is the timeout parameter of queue.get
+
+        Returns:
+            Events we got from the queue
+        """
+        result: list[str] = []
+        i: int = 0
+        while len(result) < self.queue_get_limit and i < self.queue_get_retries:
+            try:
+                result.append(self.events_queue.get(block=self.queue_get_block, timeout=self.queue_get_timeout))
+            except queue.Empty:
+                i += 1
+                self.log(message="Empty queue", level="DEBUG")
+                continue
+        return result
---
+++ Retarus/retarus_modules/consumer.py
@@ -0,0 +1,94 @@
+from queue import Queue
+from threading import Event, Thread
+from typing import Tuple, Callable
+
+import orjson
+from websocket import WebSocketApp, WebSocketTimeoutException
+
+from retarus_modules.configuration import RetarusConfig
+
+
+class RetarusEventsConsumer(Thread):
+    """Handler for receiving events from a websocket"""
+
+    def __init__(self, configuration: RetarusConfig, queue: Queue, logger: Callable, logger_exception: Callable):
+        super().__init__()
+        self.queue = queue
+        self.configuration = configuration
+        self.log = logger
+        self.log_exception = logger_exception
+
+        # Event used to stop the thread
+        self._stop_event = Event()
+
+    def stop(self):
+        """Sets the stop event"""
+        self._stop_event.set()
+
+    def create_websocket(self) -> Tuple[WebSocketApp, Thread]:
+        """Creates a WebSocket inside a Thread
+
+        Returns:
+            Tuple[WebSocketApp, Thread]: The websocket we opened, The thread in which it will run
+        """
+        websocket = WebSocketApp(
+            url=self.configuration.ws_url,
+            header=[f"Authorization: Bearer {self.configuration.ws_key}"],
+            on_message=self.on_message,
+            on_error=self.on_error,
+            on_close=self.on_close,
+        )
+        websocket_thread = Thread(target=websocket.run_forever)
+        websocket_thread.daemon = True
+
+        return websocket, websocket_thread
+
+    def on_message(self, _, event: str) -> None:
+        """Callback method called when the websocket receives a message
+
+        It will send the received message to the queue for consumption by the forwarder
+
+        Args:
+            _ (_type_): _description_
+            event (str): Message received on the websocket
+        """
+        self.queue.put(event)
+
+    def on_error(self, _, error: Exception):
+        """Callback method called when the websocket encounters an exception
+
+        We log the exception using the Connector's logger, as a warning if it is a timeout, as an error otherwise
+
+        Args:
+            _ (_type_): _description_
+            error (Exception): Exception encountered
+        """
+        if isinstance(error, WebSocketTimeoutException):
+            # add a gentler message for timeout
+            self.log(message="Websocket timed out", level="warning")
+        else:
+            self.log(message=f"Websocket error: {error}", level="error")
+
+    def on_close(self, *_):
+        """Callback method called when the websocket is closed"""
+        self.log(message="Closing socket connection", level="info")
+
+    def run(self):
+        """Start the websocket thread then wait for the stop event to be set and close the websocket when it happens"""
+        self.log(message=f"Connection to stream {self.configuration.ws_url}", level="info")
+
+        websocket, websocket_thread = self.create_websocket()
+        websocket_thread.start()
+
+        self._stop_event.wait()
+
+        websocket.close()
+
+    @property
+    def is_running(self) -> bool:
+        """Helper method to check if the stop event has been set
+
+        Returns:
+            bool: False if _stop_event is set, True otherwise
+        """
+        return not self._stop_event.is_set()
