5357d2187b9b000b83a697f93adcc3414e6ed57a
vladyslav.guriev <vladyslav.guriev@gmail.com>
2023-10-09 18:35:13 +0300

    Add logic based to CloudtrailLogs parser on #346

--- AWS/aws/s3/trigger_s3_records.py
+++ AWS/aws/s3/trigger_s3_records.py
@@ -6,13 +6,33 @@ from aws.s3.queued import AWSS3QueuedConnector
 class AWSS3RecordsTrigger(AWSS3QueuedConnector):
     name = "AWS S3 Records"
 
+    @staticmethod
+    def is_valid_payload(payload: dict) -> bool:  # pragma: no cover
+        event_source = payload.get('eventSource')
+
+        if event_source is 's3.amazonaws.com' and filter(lambda x: x in payload.get('eventName'), [
+            'Delete', 'Create', 'Copy', 'Put', 'Restore', 'GetObject', 'GetObjectTorrent', 'ListBuckets'
+        ]):
+            return True
+
+        if event_source != 's3.amazonaws.com' and filter(lambda x: x in payload.get('eventName'), [
+            'List', 'Describe'
+        ]):
+            return True
+
+        return False
+
     def _parse_content(self, content: bytes) -> list:
         if len(content) == 0:
             return []
 
         records = []
-        for record in orjson.loads(content).get("Records", []):
-            if len(record) > 0:
-                records.append(orjson.dumps(record).decode("utf-8"))
+        for data in orjson.loads(content).get("Records", []):
+            if len(data) > 0:
+                # https://docs.aws.amazon.com/awscloudtrail/latest/userguide/cloudtrail-log-file-examples.html
+                # Go through each element in list and add to result_data if it is a valid payload based on this
+                # https://github.com/SEKOIA-IO/automation-library/issues/346
+                result_data = [result for result in data if self.is_valid_payload(result)]  # pragma: no cover
+                records.append(orjson.dumps(result_data).decode("utf-8"))
 
         return records
