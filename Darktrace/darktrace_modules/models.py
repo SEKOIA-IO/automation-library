0219e4f95eeb291df57f8dfefabf67e2693f2df4
Adamowoc <oceane.adamowski@sekoia.io>
2023-10-23 17:18:37 +0200

    cert pb

--- Darktrace/darktrace_modules/models.py
+++ Darktrace/darktrace_modules/models.py
@@ -5,4 +5,4 @@ class DarktraceModuleConfiguration(BaseModel):
     api_url: str = Field(..., description="The url of the Darktrace appliance")
     public_key: str = Field(..., description="The public key to the Darktrace API")
     private_key: str = Field(secret=True, description="The private key to the Darktrace API")
-    verify_certificate: str | None = Field(..., description="Is the certificate verified ?")
+    verify_certificate: bool = Field(..., description="Is the server certificate verified")
