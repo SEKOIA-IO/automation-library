95824428fe000c9c0e0e621095b7f63a35eefdc9
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2023-10-26 14:34:44 +0200

    feat(Tehtris): allow to define an alternative url to call the Tehtris instance

--- Tehtris/tests/test_tehtris_event_trigger.py
+++ Tehtris/tests/test_tehtris_event_trigger.py
@@ -112,7 +112,7 @@ def message2():
 def test_fetch_events(trigger, message1, message2):
     with requests_mock.Mocker() as mock:
         mock.get(
-            "https://abc.api.tehtris.net/api/xdr/v1/event",
+                "https://abc.api.tehtris.net/api/xdr/v1/event",
             status_code=200,
             json=[message1, message2],
         )
@@ -125,12 +125,12 @@ def test_fetch_events_pagination(trigger, message1, message2):
     second_batch = [message2] * 25
     with requests_mock.Mocker() as mock:
         m1 = mock.get(
-            "https://abc.api.tehtris.net/api/xdr/v1/event?fromDate=1666180739&limit=100&offset=0",
+                "https://abc.api.tehtris.net/api/xdr/v1/event?fromDate=1666180739&limit=100&offset=0",
             status_code=200,
             json=first_batch,
         )
         m2 = mock.get(
-            "https://abc.api.tehtris.net/api/xdr/v1/event?fromDate=1666180739&limit=100&offset=100",
+                "https://abc.api.tehtris.net/api/xdr/v1/event?fromDate=1666180739&limit=100&offset=100",
             status_code=200,
             json=second_batch,
         )
@@ -146,7 +146,7 @@ def test_fetch_events_pagination(trigger, message1, message2):
 def test_next_batch_sleep_until_next_batch(trigger, message1, message2):
     with requests_mock.Mocker() as mock, patch("tehtris_modules.trigger_tehtris_events.time") as mock_time:
         mock.get(
-            "https://abc.api.tehtris.net/api/xdr/v1/event",
+                "https://abc.api.tehtris.net/api/xdr/v1/event",
             status_code=200,
             json=[message1, message2],
         )
@@ -164,7 +164,7 @@ def test_next_batch_sleep_until_next_batch(trigger, message1, message2):
 def test_long_next_batch_should_not_sleep(trigger, message1, message2):
     with requests_mock.Mocker() as mock, patch("tehtris_modules.trigger_tehtris_events.time") as mock_time:
         mock.get(
-            "https://abc.api.tehtris.net/api/xdr/v1/event",
+                "https://abc.api.tehtris.net/api/xdr/v1/event",
             status_code=200,
             json=[message1, message2],
         )
@@ -190,7 +190,7 @@ def test_next_batch_with_small_chunk_size(symphony_storage, message1, message2):
     trigger.configuration = {"intake_key": "intake_key", "chunk_size": 2}
     with requests_mock.Mocker() as mock, patch("tehtris_modules.trigger_tehtris_events.time") as mock_time:
         mock.get(
-            "https://abc.api.tehtris.net/api/xdr/v1/event",
+                "https://abc.api.tehtris.net/api/xdr/v1/event",
             status_code=200,
             json=[message1, message2, message1],
         )
@@ -202,3 +202,25 @@ def test_next_batch_with_small_chunk_size(symphony_storage, message1, message2):
         trigger.next_batch()
 
         assert trigger.push_events_to_intakes.call_count == 2
+
+
+def test_fetch_events_with_alternative_url(symphony_storage, patch_datetime_now, message1, message2):
+    module = TehtrisModule()
+    trigger = TehtrisEventConnector(module=module, data_path=symphony_storage)
+    # mock the log function of trigger that requires network access to the api for reporting
+    trigger.log = MagicMock()
+    trigger.log_exception = MagicMock()
+    trigger.push_events_to_intakes = MagicMock()
+    trigger.module.configuration = {"apikey": "myapikey", "tenant_id": "abc", "alternative_url": "https://bde.api.tehtris.net:8443/api/"}
+    trigger.configuration = {
+        "intake_key": "intake_key",
+        "filter_id": "uuid",
+    }
+    with requests_mock.Mocker() as mock:
+        mock.get(
+                "https://bde.api.tehtris.net:8443/api/xdr/v1/event",
+            status_code=200,
+            json=[message1, message2],
+        )
+
+        assert next(trigger.fetch_events()) == [message1, message2]
