4b859f6dfb125be0a2b1bf0627e763134ed28b2c
Adamowoc <oceane.adamowski@sekoia.io>
2023-11-08 09:41:26 +0100

    apply Olivier advice on urllib3

--- Darktrace/darktrace_modules/client/retry.py
+++ Darktrace/darktrace_modules/client/retry.py
@@ -1,5 +1,6 @@
 import time
 
+from urllib3.response import BaseHTTPResponse
 from urllib3.util.retry import Retry as BaseRetry
 
 
@@ -20,18 +21,18 @@ class Retry(BaseRetry):
 
         return None
 
-    def get_retry_after(self, response) -> float | None:
+    def get_retry_after(self, response: BaseHTTPResponse) -> float | None:
         """
         Manage Rate-limiting headers from the server.
         Support standard header Retry-After and custom header X-Rate-Limit-Reset
         """
 
         # parse Retry-After header if defined
-        retry_after = response.getheader("Retry-After")
+        retry_after = response.headers.get("Retry-After")
         if retry_after:
             return self.parse_retry_after(retry_after)
 
-        ratelimit_retry_after = response.getheader("X-Rate-Limit-Reset")
+        ratelimit_retry_after = response.headers.get("X-Rate-Limit-Reset")
         if ratelimit_retry_after:
             return self.parse_ratelimit_retry_after(ratelimit_retry_after)
 
