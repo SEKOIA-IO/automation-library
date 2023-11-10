d4744c512afdd14dd756d11b1de5cbf3006a3e91
lvoloshyn-sekoia <leonid.voloshyn@sekoia.io>
2023-11-10 16:49:44 +0200

    Add more tests

--- JIRA/tests/test_create_issue.py
+++ JIRA/tests/test_create_issue.py
@@ -1,3 +1,4 @@
+import json
 from unittest.mock import MagicMock
 
 import pytest
@@ -54,6 +55,82 @@ def create_issue_metadata_response():
                                 "key": "issuetype",
                                 "hasDefaultValue": False,
                                 "operations": ["set"],
+                            },
+                            "customfield_10036": {
+                                "required": False,
+                                "schema": {
+                                    "type": "option",
+                                    "custom": "com.atlassian.jira.plugin.system.customfieldtypes:select",
+                                    "customId": 10036,
+                                },
+                                "name": "Some Field Name",
+                                "key": "customfield_10036",
+                                "hasDefaultValue": False,
+                                "operations": ["set"],
+                                "allowedValues": [
+                                    {
+                                        "self": "https://test.atlassian.net/rest/api/3/customFieldOption/10020",
+                                        "value": "Option 1",
+                                        "id": "10020",
+                                    },
+                                    {
+                                        "self": "https://test.atlassian.net/rest/api/3/customFieldOption/10021",
+                                        "value": "Option 2",
+                                        "id": "10021",
+                                    },
+                                ],
+                            },
+                            "customfield_10037": {
+                                "required": False,
+                                "schema": {
+                                    "type": "array",
+                                    "items": "option",
+                                    "custom": "com.atlassian.jira.plugin.system.customfieldtypes:multicheckboxes",
+                                    "customId": 10037,
+                                },
+                                "name": "Some Checkbox",
+                                "key": "customfield_10037",
+                                "hasDefaultValue": False,
+                                "operations": ["add", "set", "remove"],
+                                "allowedValues": [
+                                    {
+                                        "self": "https://test.atlassian.net/rest/api/3/customFieldOption/10022",
+                                        "value": "C1",
+                                        "id": "10022",
+                                    },
+                                    {
+                                        "self": "https://test.atlassian.net/rest/api/3/customFieldOption/10023",
+                                        "value": "C2",
+                                        "id": "10023",
+                                    },
+                                ],
+                            },
+                            "customfield_10040": {
+                                "required": False,
+                                "schema": {
+                                    "type": "date",
+                                    "custom": "com.atlassian.jira.plugin.system.customfieldtypes:datepicker",
+                                    "customId": 10040,
+                                },
+                                "name": "Some Date",
+                                "key": "customfield_10040",
+                                "hasDefaultValue": False,
+                                "operations": ["set"],
+                            },
+                            "customfield_10043": {
+                                "required": False,
+                                "schema": {
+                                    "type": "array",
+                                    "items": "user",
+                                    "custom": "com.atlassian.jira.plugin.system.customfieldtypes:people",
+                                    "customId": 10043,
+                                    "configuration": {"isMulti": False},
+                                },
+                                "name": "Some User Field",
+                                "key": "customfield_10043",
+                                "autoCompleteUrl": "https://test.atlassian.net/rest/api/1.0/users/picker?fieldName=customfield_10043&showAvatar=true&query=",
+                                "hasDefaultValue": False,
+                                "operations": ["add", "set", "remove"],
                             },
                             "priority": {
                                 "required": False,
@@ -194,7 +271,7 @@ def test_create_issue_no_project(action: JIRACreateIssue) -> None:
             labels="dev,cloud9",
             assignee="John Doe",
             reporter="Jane Doe",
-            priority="Major",
+            priority="Highest",
             parent_key=None,
             custom_fields=None,
             description=None,
@@ -264,7 +341,7 @@ def test_create_issue_with_non_existent_priority(
             labels="dev,cloud9",
             assignee="John Doe",
             reporter="Jane Doe",
-            priority="HIGHEST",
+            priority="Major",
             parent_key=None,
             custom_fields=None,
             description=None,
@@ -286,11 +363,204 @@ def test_incorrect_credentials(action: JIRACreateIssue):
             labels="dev,cloud9",
             assignee="John Doe",
             reporter="Jane Doe",
-            priority="HIGHEST",
+            priority="Highest",
             parent_key=None,
             custom_fields=None,
             description=None,
         )
         with pytest.raises(HTTPError):
+            result = action.run(args)
+            assert result is None
+
+
+def test_custom_field_text_value(
+    action: JIRACreateIssue, create_issue_metadata_response: dict, get_all_users_response: dict
+):
+    with requests_mock.Mocker() as mock:
+        mock.register_uri("POST", "https://test.atlassian.net/rest/api/3/issue", json={"key": "PRJ-1"})
+        mock.register_uri(
+            "GET", "https://test.atlassian.net/rest/api/3/issue/createmeta", json=create_issue_metadata_response
+        )
+        mock.register_uri(
+            "GET",
+            "https://test.atlassian.net/rest/api/3/users/search",
+            [
+                {"json": get_all_users_response},
+                {"json": []},
+            ],
+        )
+
+        args = JiraCreateIssueArguments(
+            project_key="PRJ",
+            summary="New Task",
+            issue_type="Bug",
+            due_date="2077-10-23",
+            labels="dev,cloud9",
+            assignee="John Doe",
+            reporter="Jane Doe",
+            priority="Highest",
+            parent_key=None,
+            custom_fields=json.dumps({"Some Field Name": "Option 1"}),
+            description=None,
+        )
+        result = action.run(args)
+        assert result is not None
+
+    with requests_mock.Mocker() as mock:
+        mock.register_uri(
+            "GET", "https://test.atlassian.net/rest/api/3/issue/createmeta", json=create_issue_metadata_response
+        )
+        mock.register_uri(
+            "GET",
+            "https://test.atlassian.net/rest/api/3/users/search",
+            [
+                {"json": get_all_users_response},
+                {"json": []},
+            ],
+        )
+
+        args = JiraCreateIssueArguments(
+            project_key="PRJ",
+            summary="New Task",
+            issue_type="Bug",
+            due_date="2077-10-23",
+            labels="dev,cloud9",
+            assignee="John Doe",
+            reporter="Jane Doe",
+            priority="Highest",
+            parent_key=None,
+            custom_fields=json.dumps({"Some Field Name": "Option 3"}),  # non-existent
+            description=None,
+        )
+        with pytest.raises(ValueError):
+            result = action.run(args)
+            assert result is None
+
+
+def test_custom_select_field_value(
+    action: JIRACreateIssue, create_issue_metadata_response: dict, get_all_users_response: dict
+):
+    with requests_mock.Mocker() as mock:
+        mock.register_uri("POST", "https://test.atlassian.net/rest/api/3/issue", json={"key": "PRJ-1"})
+        mock.register_uri(
+            "GET", "https://test.atlassian.net/rest/api/3/issue/createmeta", json=create_issue_metadata_response
+        )
+        mock.register_uri(
+            "GET",
+            "https://test.atlassian.net/rest/api/3/users/search",
+            [
+                {"json": get_all_users_response},
+                {"json": []},
+            ],
+        )
+
+        args = JiraCreateIssueArguments(
+            project_key="PRJ",
+            summary="New Task",
+            issue_type="Bug",
+            due_date="2077-10-23",
+            labels="dev,cloud9",
+            assignee="John Doe",
+            reporter="Jane Doe",
+            priority="Highest",
+            parent_key=None,
+            custom_fields=json.dumps({"Some Checkbox": ["C1", "C2"]}),
+            description=None,
+        )
+        result = action.run(args)
+        assert result is not None
+
+    with requests_mock.Mocker() as mock:
+        mock.register_uri(
+            "GET", "https://test.atlassian.net/rest/api/3/issue/createmeta", json=create_issue_metadata_response
+        )
+        mock.register_uri(
+            "GET",
+            "https://test.atlassian.net/rest/api/3/users/search",
+            [
+                {"json": get_all_users_response},
+                {"json": []},
+            ],
+        )
+
+        args = JiraCreateIssueArguments(
+            project_key="PRJ",
+            summary="New Task",
+            issue_type="Bug",
+            due_date="2077-10-23",
+            labels="dev,cloud9",
+            assignee="John Doe",
+            reporter="Jane Doe",
+            priority="Highest",
+            parent_key=None,
+            custom_fields=json.dumps({"Some Checkbox": ["C3", "C1"]}),  # C3 does not exist
+            description=None,
+        )
+        with pytest.raises(ValueError):
+            result = action.run(args)
+            assert result is None
+
+
+def test_custom_user_list_field_value(
+    action: JIRACreateIssue, create_issue_metadata_response: dict, get_all_users_response: dict
+):
+    with requests_mock.Mocker() as mock:
+        mock.register_uri("POST", "https://test.atlassian.net/rest/api/3/issue", json={"key": "PRJ-1"})
+        mock.register_uri(
+            "GET", "https://test.atlassian.net/rest/api/3/issue/createmeta", json=create_issue_metadata_response
+        )
+        mock.register_uri(
+            "GET",
+            "https://test.atlassian.net/rest/api/3/users/search",
+            [
+                {"json": get_all_users_response},
+                {"json": []},
+            ],
+        )
+
+        args = JiraCreateIssueArguments(
+            project_key="PRJ",
+            summary="New Task",
+            issue_type="Bug",
+            due_date="2077-10-23",
+            labels="dev,cloud9",
+            assignee="John Doe",
+            reporter="Jane Doe",
+            priority="Highest",
+            parent_key=None,
+            custom_fields=json.dumps({"Some User Field": ["John Doe"]}),
+            description=None,
+        )
+        result = action.run(args)
+        assert result is not None
+
+    with requests_mock.Mocker() as mock:
+        mock.register_uri("POST", "https://test.atlassian.net/rest/api/3/issue", json={"key": "PRJ-1"})
+        mock.register_uri(
+            "GET", "https://test.atlassian.net/rest/api/3/issue/createmeta", json=create_issue_metadata_response
+        )
+        mock.register_uri(
+            "GET",
+            "https://test.atlassian.net/rest/api/3/users/search",
+            [
+                {"json": get_all_users_response},
+                {"json": []},
+            ],
+        )
+
+        args = JiraCreateIssueArguments(
+            project_key="PRJ",
+            summary="New Task",
+            issue_type="Bug",
+            due_date="2077-10-23",
+            labels="dev,cloud9",
+            assignee="John Doe",
+            reporter="Jane Doe",
+            priority="Highest",
+            parent_key=None,
+            custom_fields=json.dumps({"Some User Field": ["John D'oh"]}),
+            description=None,
+        )
+        with pytest.raises(ValueError):
             result = action.run(args)
             assert result is None
