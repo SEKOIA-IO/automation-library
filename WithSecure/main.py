998b329e16e0725d92b2601d11ef8e786fc8b23b
fspms <32630772+fspms@users.noreply.github.com>
2023-11-22 11:27:44 +0100

    WithSecure : Update main.py
    Add assign profile action

--- WithSecure/main.py
+++ WithSecure/main.py
@@ -4,6 +4,7 @@ from withsecure.list_devices_action import ListDevicesAction
 from withsecure.release_device_from_network_isolation_action import ReleaseDeviceFromNetworkIsolationAction
 from withsecure.scan_device_for_malware import ScanDeviceForMalware
 from withsecure.security_events_connector import SecurityEventsConnector
+from withsecure.assign_device_profile_action import AssignDeviceProfileAction
 
 if __name__ == "__main__":
     module = WithSecureModule()
@@ -11,5 +12,6 @@ if __name__ == "__main__":
     module.register(IsolateDeviceFromNetworkAction, "isolate_device_from_network")
     module.register(ReleaseDeviceFromNetworkIsolationAction, "release_device_from_network_isolation")
     module.register(ScanDeviceForMalware, "scan_device_for_malware")
+    module.register(AssignDeviceProfileAction, "assign_device_profile")
     module.register(ListDevicesAction, "list_devices")
     module.run()
