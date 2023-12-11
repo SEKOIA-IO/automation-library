5218e943a52e1b6264c5d7e32469cda8809704a2
Antoine Ryon <antoine.ryon@gmail.com>
2023-12-11 10:41:47 +0100

    Improve tests

--- Sekoia.io/tests/test_add_ioc_2_ioc_collection.py
+++ Sekoia.io/tests/test_add_ioc_2_ioc_collection.py
@@ -5,16 +5,29 @@ from sekoiaio.intelligence_center.add_ioc_to_ioc_collection import AddIOCtoIOCCo
 
 
 @pytest.fixture
-def add_ioc_response():
-    return {"task_id": "00000000-0000-0000-0000-000000000000"}
-
-
-def add_ioc(add_ioc_response):
-    arguments = {
+def arguments_success():
+    return {
         "indicators": "8.8.8.8",
         "ioc_collection_id": "ioc-collection--00000000-0000-0000-0000-000000000000",
         "indicator_type": "ipv4-addr.value",
     }
+
+
+@pytest.fixture
+def arguments_failure():
+    return {
+        "indicators": "8.8.8.8",
+        "ioc_collection_id": "ioc-collection--00000000-0000-0000-0000-000000000000",
+        "indicator_type": "invalid",
+    }
+
+
+@pytest.fixture
+def add_ioc_response():
+    return {"task_id": "00000000-0000-0000-0000-000000000000"}
+
+
+def add_ioc(arguments_success, add_ioc_response):
     action: AddIOCtoIOCCollectionAction = AddIOCtoIOCCollectionAction()
     action.module.configuration = {"base_url": "http://fake.url/", "api_key": "fake_api_key"}
 
@@ -25,7 +38,7 @@ def add_ioc(add_ioc_response):
             "http://fake.url/api/v2/inthreat/ioc-collections/ioc-collection--00000000-0000-0000-0000-000000000000/indicators/text",
             json=expected_response,
         )
-        results: dict = action.run(arguments)
+        results: dict = action.run(arguments_success)
 
         assert results == add_ioc_response
         assert mock.call_count == 1
@@ -33,12 +46,7 @@ def add_ioc(add_ioc_response):
         assert history[0].method == "POST"
 
 
-def add_ioc_failed(add_ioc_response):
-    arguments = {
-        "indicators": "8.8.8.8",
-        "ioc_collection_id": "ioc-collection--00000000-0000-0000-0000-000000000000",
-        "indicator_type": "invalid",
-    }
+def add_ioc_failed(arguments_failure, add_ioc_response):
     action: AddIOCtoIOCCollectionAction = AddIOCtoIOCCollectionAction()
     action.module.configuration = {"base_url": "http://fake.url/", "api_key": "fake_api_key"}
 
@@ -49,7 +57,7 @@ def add_ioc_failed(add_ioc_response):
             "http://fake.url/api/v2/inthreat/ioc-collections/ioc-collection--00000000-0000-0000-0000-000000000000/indicators/text",
             status_code=400,
         )
-        results: dict = action.run(arguments)
+        results: dict = action.run(arguments_failure)
 
         assert result["status_code"] == 400
         assert mock.call_count == 1
