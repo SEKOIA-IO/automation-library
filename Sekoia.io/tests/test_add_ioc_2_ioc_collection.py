c73a8a92970eeffb212701979324cb4250addc78
Antoine Ryon <antoine.ryon@gmail.com>
2023-12-11 09:21:27 +0100

    Improve code coverage

--- Sekoia.io/tests/test_add_ioc_2_ioc_collection.py
+++ Sekoia.io/tests/test_add_ioc_2_ioc_collection.py
@@ -31,3 +31,25 @@ def add_ioc(add_ioc_response):
         assert mock.call_count == 1
         history = mock.request_history
         assert history[0].method == "POST"
+
+
+def add_ioc_failed(add_ioc_response):
+    arguments = {
+        "indicators": "8.8.8.8",
+        "ioc_collection_id": "ioc-collection--00000000-0000-0000-0000-000000000000",
+        "indicator_type": "invalid",
+    }
+    action: AddIOCtoIOCCollectionAction = AddIOCtoIOCCollectionAction()
+    action.module.configuration = {"base_url": "http://fake.url/", "api_key": "fake_api_key"}
+
+    expected_response = add_ioc_response
+
+    with requests_mock.Mocker() as mock:
+        mock.post(
+            "http://fake.url/api/v2/inthreat/ioc-collections/ioc-collection--00000000-0000-0000-0000-000000000000/indicators/text",
+            status_code=400,
+        )
+        results: dict = action.run(arguments)
+
+        assert result["status_code"] == 400
+        assert mock.call_count == 1
