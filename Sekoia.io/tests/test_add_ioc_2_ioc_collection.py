579777b22ee827cb9c62518da4abe9c42c953a47
Raphael Cohen <raphael.cohen@sekoia.fr>
2023-12-12 14:21:09 +0100

    chore(Sekoia.io): Fixes tests

--- Sekoia.io/tests/test_add_ioc_2_ioc_collection.py
+++ Sekoia.io/tests/test_add_ioc_2_ioc_collection.py
@@ -22,42 +22,33 @@ def arguments_failure():
     }
 
 
-@pytest.fixture
-def add_ioc_response():
-    return {"task_id": "00000000-0000-0000-0000-000000000000"}
-
-
-def add_ioc(arguments_success, add_ioc_response):
+def test_add_ioc(arguments_success):
     action: AddIOCtoIOCCollectionAction = AddIOCtoIOCCollectionAction()
     action.module.configuration = {"base_url": "http://fake.url/", "api_key": "fake_api_key"}
 
-    expected_response = add_ioc_response
-
+    response = {"task_id": "00000000-0000-0000-0000-000000000000"}
     with requests_mock.Mocker() as mock:
         mock.post(
             "http://fake.url/api/v2/inthreat/ioc-collections/ioc-collection--00000000-0000-0000-0000-000000000000/indicators/text",
-            json=expected_response,
+            json=response,
         )
-        results: dict = action.run(arguments_success)
+        action.run(arguments_success)
 
-        assert results == add_ioc_response
         assert mock.call_count == 1
         history = mock.request_history
         assert history[0].method == "POST"
 
 
-def add_ioc_failed(arguments_failure, add_ioc_response):
+def test_add_ioc_failed(arguments_failure):
     action: AddIOCtoIOCCollectionAction = AddIOCtoIOCCollectionAction()
     action.module.configuration = {"base_url": "http://fake.url/", "api_key": "fake_api_key"}
-
-    expected_response = add_ioc_response
 
     with requests_mock.Mocker() as mock:
         mock.post(
             "http://fake.url/api/v2/inthreat/ioc-collections/ioc-collection--00000000-0000-0000-0000-000000000000/indicators/text",
             status_code=400,
         )
-        results: dict = action.run(arguments_failure)
+        action.run(arguments_failure)
 
-        assert result["status_code"] == 400
         assert mock.call_count == 1
+        assert action._error is not None
