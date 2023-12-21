8403c823a5398f359b0204ad1fb4bd9fd41aeb61
vladyslav.guriev <vladyslav.guriev@gmail.com>
2023-12-21 11:23:08 +0200

    Hotfix: Module name in WindowsServerActions

--- MicrosoftWindowsServer/main.py
+++ MicrosoftWindowsServer/main.py
@@ -1,13 +1,12 @@
 """Module to work with windows remote server."""
 
-from sekoia_automation.module import Module
-
+from actions import MicrosoftModule
 from actions.change_user_password_action import ChangeUserPasswordAction
 from actions.disable_users_action import DisableUsersAction
 from actions.enable_users_action import EnableUsersAction
 
 if __name__ == "__main__":
-    module = Module()
+    module = MicrosoftModule()
 
     module.register(ChangeUserPasswordAction, "change-user-password")
     module.register(EnableUsersAction, "enable-users")
