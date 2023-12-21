cff5de1dd97b5c705462566270b6cb9b9aa5fbbf
rombernier <136727505+rombernier@users.noreply.github.com>
2023-12-21 17:39:26 +0100

    Update Zscaler/main.py
    Co-authored-by: Sébastien Quioc <sebastien.quioc@sekoia.fr>

--- Zscaler/main.py
+++ Zscaler/main.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     module = Module()
     module.register(ZscalerBlockIOC, "zscaler_block_ioc")
     module.register(ZscalerPushIOCBlock, "zscaler_push_iocs_block")
+    module.register(ZscalerUnBlockIOC, "zscaler_unblock_ioc")
 
     module.run()
