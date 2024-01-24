c83be0244153453f0ac653769aaac6423a44e818
vladyslav.guriev <vladyslav.guriev@gmail.com>
2024-01-24 13:35:36 +0200

    Salesforce Rate Limit hotfix

--- Salesforce/salesforce/models.py
+++ Salesforce/salesforce/models.py
@@ -9,6 +9,14 @@ class SalesforceModuleConfig(BaseModel):
     client_id: str = Field(required=True, description="Salesforce client id")
     base_url: HttpUrl = Field(required=True, description="Salesforce auth url")
     org_type: str = Field(default="production", required=True, description="Salesforce org type")
+    rate_limit: str | None = Field(
+        description=(
+            "Rate limit for requests to salesforce."
+            "Value should have next format {max_rate}/{time_period}. For example: 3/60"
+            "If value is empty, Sekoia will use default rate limits. More information you can find in docs:"
+            "https://developer.salesforce.com/docs/atlas.en-us.salesforce_app_limits_cheatsheet.meta/salesforce_app_limits_cheatsheet/salesforce_app_limits_platform_api.htm"
+        )
+    )
 
     @property
     def rate_limiter(self) -> AsyncLimiter:
@@ -22,8 +30,14 @@ class SalesforceModuleConfig(BaseModel):
         Docs: https://developer.salesforce.com/docs/atlas.en-us.salesforce_app_limits_cheatsheet.meta/salesforce_app_limits_cheatsheet/salesforce_app_limits_platform_api.htm
 
         Returns:
-            int:
+            AsyncLimiter:
         """
+        if self.rate_limit is not None and "/" in self.rate_limit:
+            max_rate = float(self.rate_limit.split("/")[0])
+            time_period = float(self.rate_limit.split("/")[-1])
+
+            return AsyncLimiter(max_rate, time_period)
+
         match self.org_type:
             case "production":
                 return AsyncLimiter(25, 20)
