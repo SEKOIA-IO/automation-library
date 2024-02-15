973ab2764a59b2734ea56d0dff9671d2b88e8c53
Jean GOUDY <jean.goudy@sekoia.io>
2024-02-15 13:53:54 +0100

    fix(deep-visibility): mypy compliant

--- SentinelOne/sentinelone_module/deep_visibility/query.py
+++ SentinelOne/sentinelone_module/deep_visibility/query.py
@@ -1,17 +1,22 @@
 from datetime import datetime
+from typing import Optional
 
 from management.mgmtsdk_v2.entities.deep_visibility_v2 import DvQuery
 from pydantic import BaseModel, Field
-from tenacity import Retrying, stop_after_delay, wait_exponential, retry_if_not_exception_type
-from typing import Optional
+from tenacity import (
+    Retrying,
+    retry_if_not_exception_type,
+    stop_after_delay,
+    wait_exponential,
+)
 
 from sentinelone_module.base import SentinelOneAction
 from sentinelone_module.exceptions import (
     QueryDeepVisibilityCanceledError,
     QueryDeepVisibilityError,
     QueryDeepVisibilityFailedError,
-    QueryDeepVisibilityTimeoutError,
     QueryDeepVisibilityRunningError,
+    QueryDeepVisibilityTimeoutError,
 )
 from sentinelone_module.helpers import to_rfc3339
 
@@ -142,7 +147,12 @@ class DeepVisibilityEvents(BaseModel):
     events: list[DeepVisibilityEvent]
 
 
-IN_PROGRESS_QUERY_STATUSES = {"RUNNING", "EVENTS_RUNNING", "QUERY_RUNNING", "PROCESS_RUNNING"}
+IN_PROGRESS_QUERY_STATUSES = {
+    "RUNNING",
+    "EVENTS_RUNNING",
+    "QUERY_RUNNING",
+    "PROCESS_RUNNING",
+}
 FINALIZED_QUERY_STATUSES = {"EMPTY_RESULTS", "FINISHED"}
 CANCELED_QUERY_STATUSES = {"QUERY_CANCEL", "QUERY_EXPIRED", "TIMEOUT"}
 FAILED_QUERY_STATUSES = {"QUERY_NOT_FOUND", "FAILED", "FAILED_CLIENT"}
@@ -159,7 +169,9 @@ class QueryDeepVisibilityAction(SentinelOneAction):
                 stop=stop_after_delay(timeout),
                 wait=wait_exponential(multiplier=1, min=1, max=10),
                 reraise=True,
-                retry=retry_if_not_exception_type(QueryDeepVisibilityCanceledError | QueryDeepVisibilityFailedError),
+                retry=retry_if_not_exception_type((
+                    QueryDeepVisibilityCanceledError, QueryDeepVisibilityFailedError
+                )),
             ):
                 with attempt:
                     result = self.client.deep_visibility_v2.get_query_status(query_id)
@@ -167,11 +179,17 @@ class QueryDeepVisibilityAction(SentinelOneAction):
                     if result.data.responseState in FINALIZED_QUERY_STATUSES:
                         return
                     elif result.data.responseState in IN_PROGRESS_QUERY_STATUSES:
-                        raise QueryDeepVisibilityRunningError(f"status {result.data.responseState}")
+                        raise QueryDeepVisibilityRunningError(
+                            f"status {result.data.responseState}"
+                        )
                     elif result.data.responseState in CANCELED_QUERY_STATUSES:
-                        raise QueryDeepVisibilityCanceledError(result.json["data"].get("responseError"))
+                        raise QueryDeepVisibilityCanceledError(
+                            result.json["data"].get("responseError")
+                        )
                     elif result.data.responseState in FAILED_QUERY_STATUSES:
-                        raise QueryDeepVisibilityFailedError(result.json["data"].get("responseError"))
+                        raise QueryDeepVisibilityFailedError(
+                            result.json["data"].get("responseError")
+                        )
         except QueryDeepVisibilityRunningError:
             raise QueryDeepVisibilityTimeoutError(timeout)
 
