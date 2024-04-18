1c8318ee8f8c5a648ce5caa0d03892469f76fe8f
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2024-04-18 17:45:36 +0200

    fix(CrowdStrike): fix the condition to stop the pagination

--- CrowdStrikeFalcon/crowdstrike_falcon/client/__init__.py
+++ CrowdStrikeFalcon/crowdstrike_falcon/client/__init__.py
@@ -78,7 +78,7 @@ class ApiClient(requests.Session):
             yield from content.get("resources") or []
 
             pagination = content.get("meta", {}).get("pagination")
-            still_fetching_items = pagination is not None and pagination.get("offset", 0) > 0
+            still_fetching_items = pagination is not None and (bool(pagination.get("offset")) or bool(pagination.get("after")))
 
 
 class CrowdstrikeFalconClient(ApiClient):
