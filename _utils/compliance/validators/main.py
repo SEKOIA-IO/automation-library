7fcb77aad218d75168a9a6d77504531c741af559
lvoloshyn-sekoia <leonid.voloshyn@sekoia.io>
2024-07-05 12:39:15 +0300

    Check for duplicated `docker_parameters` in main.py

--- _utils/compliance/validators/main.py
+++ _utils/compliance/validators/main.py
@@ -22,7 +22,7 @@ class MainPYValidator(Validator):
             )
             return
 
-        used_docker_params = set()
+        used_docker_params = []
 
         calls = list(
             search_python_files(
@@ -40,18 +40,32 @@ class MainPYValidator(Validator):
             if len(ast_node.args) > 1:
                 arg_node = ast_node.args[1]
                 arg_node_value = arg_node.value
-                used_docker_params.add(arg_node_value)
+                used_docker_params.append(arg_node_value)
 
             elif len(ast_node.keywords) > 0:
                 kwarg_node = ast_node.keywords[0]
                 kwarg_node_name = kwarg_node.arg
                 if kwarg_node_name == "name":
                     kwarg_node_value = kwarg_node.value.value
-                    used_docker_params.add(kwarg_node_value)
+                    used_docker_params.append(kwarg_node_value)
+
+        if len(used_docker_params) != len(set(used_docker_params)):
+            tmp = set()
+            duplicated_docker_params = [
+                x for x in used_docker_params if x in tmp or tmp.add(x)
+            ]
+
+            for item in duplicated_docker_params:
+                result.errors.append(
+                    CheckError(
+                        filepath=main_path,
+                        error=f"Docker parameter `{item}` used multiple times in main.py",
+                    )
+                )
 
         docker_params_in_jsons = set(result.options.get("docker_parameters").values())
-        absent_in_main_py = docker_params_in_jsons - used_docker_params
-        absent_in_jsons = used_docker_params - docker_params_in_jsons
+        absent_in_main_py = docker_params_in_jsons - set(used_docker_params)
+        absent_in_jsons = set(used_docker_params) - docker_params_in_jsons
 
         for item in absent_in_main_py:
             result.errors.append(
