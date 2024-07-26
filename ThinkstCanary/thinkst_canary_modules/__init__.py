cee08f2416a3595de49458e901c0504c6cbfc4c2
Sébastien Quioc <sebastien.quioc@sekoia.fr>
2024-07-26 13:56:13 +0200

    Revert "Add Thinkst Canary module"

--- ThinkstCanary/thinkst_canary_modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from sekoia_automation.module import Module
-
-from thinkst_canary_modules.models import ThinkstCanaryModuleConfiguration
-
-
-class ThinkstCanaryModule(Module):
-    configuration: ThinkstCanaryModuleConfiguration
--- ThinkstCanary/thinkst_canary_modules/models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class ThinkstCanaryModuleConfiguration(BaseModel):
-    base_url: str = Field(..., description="Base URL")
-    auth_token: str = Field(secret=True, description="Auth token")
