940f5bf8d795789359da02755d1ca4d74b0d93fa
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2024-07-29 16:33:58 +0200

    feat(Trellix): add a retry option that handle HTTP 429 responses

---
+++ Trellix/client/retry.py
@@ -0,0 +1,32 @@
+from aiohttp_retry import RetryOptionsBase
+from aiohttp import ClientResponse
+
+
+class RetryWithRateLimiter(RetryOptionsBase):
+    """
+    This retry option will pause the retry according to the Retry-After, if defined, when the limit was exhausted (status code: 429).
+    Otherwise, it will call the underlying retry options
+    """
+
+    RETRY_AFTER_HEADER = "Retry-After"
+
+    def __init__(self, retry_options: RetryOptionsBase):
+        self._wrapped_retry_options = retry_options
+
+    def __getattr__(self, name):
+        return getattr(self._wrapped_retry_options, name)
+
+    def get_timeout(self, attempt: int, response: ClientResponse | None = None) -> float:
+        """
+        Return the time to wait before the next attempt
+
+        Args:
+            attempt: The number of attempts for the request
+            response: The response of the previous attempt
+        """
+        # Call the underlying retry options if the response is not defined or if the response is not a too many requests response (status code: 429)
+        if response is None or response.status != 429 or self.RETRY_AFTER_HEADER not in response.headers:
+            return self._wrapped_retry_options.get_timeout(attempt, response)
+
+        # Return the pause specified in the Retry-After header
+        return int(response.headers["Retry-After"])
---
+++ Trellix/tests/client/test_retry.py
@@ -0,0 +1,49 @@
+from unittest.mock import MagicMock
+
+from client.retry import RetryWithRateLimiter
+
+
+def test_retry_with_success_request_should_call_underlying_retry_options():
+    retry_options = MagicMock()
+    attempt = 1
+    response = MagicMock()
+    response.status = 200
+    retry = RetryWithRateLimiter(retry_options=retry_options)
+
+    retry.get_timeout(1, response)
+    assert retry_options.get_timeout.called
+
+
+def test_retry_none_response_should_call_underlying_retry_options():
+    retry_options = MagicMock()
+    attempt = 1
+    response = None
+    retry = RetryWithRateLimiter(retry_options=retry_options)
+
+    retry.get_timeout(1, response)
+    assert retry_options.get_timeout.called
+
+
+def test_retry_without_retry_after_should_call_underlying_retry_options():
+    retry_options = MagicMock()
+    attempt = 1
+    response = MagicMock()
+    response.status = 429
+    response.headers = {}
+    retry = RetryWithRateLimiter(retry_options=retry_options)
+
+    retry.get_timeout(1, response)
+    assert retry_options.get_timeout.called
+
+
+def test_retry_with_exhausted_limit_should_wait_retry_after():
+    retry_options = MagicMock()
+    attempt = 1
+    response = MagicMock()
+    response.status = 429
+    response.headers = {"Retry-After": 250}
+    retry = RetryWithRateLimiter(retry_options=retry_options)
+
+    time_to_sleep = retry.get_timeout(1, response)
+    assert not retry_options.get_timeout.called
+    assert time_to_sleep == 250
