2fe2402a1ea878bac5abc91cda92a04a2e42464b
vladyslav.guriev <vladyslav.guriev@gmail.com>
2024-09-09 15:43:42 +0300

    Feature: Add support of isolate and deisolate host in Crowdstrike Falcon

--- CrowdStrikeFalcon/main.py
+++ CrowdStrikeFalcon/main.py
@@ -6,6 +6,7 @@ from crowdstrike_falcon.custom_iocs import (
     CrowdstrikeActionPushIOCsDetect,
 )
 from crowdstrike_falcon.event_stream_trigger import EventStreamTrigger
+from crowdstrike_falcon.host_actions import CrowdstrikeActionDeIsolateHosts, CrowdstrikeActionIsolateHosts
 
 if __name__ == "__main__":
     module = CrowdStrikeFalconModule()
@@ -14,5 +15,7 @@ if __name__ == "__main__":
     module.register(CrowdstrikeActionPushIOCsDetect, "push_iocs_detect")
     module.register(CrowdstrikeActionBlockIOC, "block_ioc")
     module.register(CrowdstrikeActionMonitorIOC, "monitor_ioc")
+    module.register(CrowdstrikeActionIsolateHosts, "isolate_hosts")
+    module.register(CrowdstrikeActionDeIsolateHosts, "deisolate_hosts")
 
     module.run()
