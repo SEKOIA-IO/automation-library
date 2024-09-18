dab19c701f74ee2fec1b0d2067f2275c0e417e0b
vladyslav.guriev <vladyslav.guriev@gmail.com>
2024-09-18 11:02:14 +0300

    Push review comments

--- Trellix/connectors/trellix_epo_connector.py
+++ Trellix/connectors/trellix_epo_connector.py
@@ -118,43 +118,42 @@ class TrellixEpoConnector(AsyncConnector):
         previous_processing_end: float | None = None
 
         while self.running:
-            processing_start = time.time()
-            if previous_processing_end is not None:
-                EVENTS_LAG.labels(intake_key=self.configuration.intake_key).set(
-                    processing_start - previous_processing_end
+            try:
+                processing_start = time.time()
+                if previous_processing_end is not None:
+                    EVENTS_LAG.labels(intake_key=self.configuration.intake_key).set(
+                        processing_start - previous_processing_end
+                    )
+
+                message_ids: list[str] = await self.get_trellix_epo_events()
+                processing_end = time.time()
+                OUTCOMING_EVENTS.labels(intake_key=self.configuration.intake_key).inc(len(message_ids))
+
+                log_message = "No records to forward"
+                if len(message_ids) > 0:
+                    log_message = "Pushed {0} records".format(len(message_ids))
+
+                self.log(message=log_message, level="info")
+
+                processing_time = processing_end - processing_start
+                logger.info(
+                    "Processing took {processing_time} seconds",
+                    processing_time=processing_time,
                 )
 
-            message_ids: list[str] = await self.get_trellix_epo_events()
-            processing_end = time.time()
-            OUTCOMING_EVENTS.labels(intake_key=self.configuration.intake_key).inc(len(message_ids))
+                FORWARD_EVENTS_DURATION.labels(intake_key=self.configuration.intake_key).observe(processing_time)
 
-            log_message = "No records to forward"
-            if len(message_ids) > 0:
-                log_message = "Pushed {0} records".format(len(message_ids))
+                # compute the remaining sleeping time. If greater than 0 and no messages where fetched, pause the connector
+                delta_sleep = self.configuration.frequency - processing_time
+                if len(message_ids) == 0 and delta_sleep > 0:
+                    self.log(message=f"Next batch in the future. Waiting {delta_sleep} seconds", level="info")
+                    await asyncio.sleep(delta_sleep)
 
-            self.log(message=log_message, level="info")
-
-            processing_time = processing_end - processing_start
-            logger.info(
-                "Processing took {processing_time} seconds",
-                processing_time=processing_time,
-            )
-
-            FORWARD_EVENTS_DURATION.labels(intake_key=self.configuration.intake_key).observe(processing_time)
-
-            # compute the remaining sleeping time. If greater than 0 and no messages where fetched, pause the connector
-            delta_sleep = self.configuration.frequency - processing_time
-            if len(message_ids) == 0 and delta_sleep > 0:
-                self.log(message=f"Next batch in the future. Waiting {delta_sleep} seconds", level="info")
-                await asyncio.sleep(delta_sleep)
+            except Exception as e:
+                self.log_exception(e, message="Error while running Trellix EPO")
+                await asyncio.sleep(self.configuration.frequency)
 
     def run(self) -> None:  # pragma: no cover
         """Runs TrellixEdr."""
         loop = asyncio.get_event_loop()
-
-        while self.running:
-            try:
-                loop.run_until_complete(self.async_run())
-            except Exception as e:
-                self.log_exception(e, message="Error while running Trellix EPO")
-                loop.run_until_complete(asyncio.sleep(self.configuration.frequency))
+        loop.run_until_complete(self.async_run())
