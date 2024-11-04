6a449193951871c9a2199acbdd8a9b2489019bd3
Gael Muller <muller.gael@gmail.com>
2024-11-04 11:15:31 +0100

    Revert "chore: Use status title instead of uuid when changing alert status"

--- Sekoia.io/sekoiaio/operation_center/__init__.py
+++ Sekoia.io/sekoiaio/operation_center/__init__.py
@@ -8,6 +8,16 @@ PatchAlert = type(
     {"verb": "patch", "endpoint": base_url + "alerts/{uuid}", "query_parameters": []},
 )
 
+TriggerActionOnAlertWorkflow = type(
+    "TriggerActionOnAlertWorkflow",
+    (GenericAPIAction,),
+    {
+        "verb": "patch",
+        "endpoint": base_url + "alerts/{uuid}/workflow",
+        "query_parameters": [],
+    },
+)
+
 CreateNewIncident = type(
     "CreateNewIncident",
     (GenericAPIAction,),
