3bc384f46184a9f963191ec035ce3f843d3baa31
lvoloshyn-sekoia <leonid.voloshyn@sekoia.io>
2024-12-02 12:19:23 +0200

    Rename connector

--- TrendMicro/main.py
+++ TrendMicro/main.py
@@ -1,9 +1,9 @@
 from trendmicro_modules import TrendMicroModule
 from trendmicro_modules.trigger_email_sec import TrendMicroEmailSecurityConnector
-from trendmicro_modules.trigger_vision import TrendMicroVisionOneConnector
+from trendmicro_modules.trigger_vision_one_workbench import TrendMicroVisionOneWorkbenchConnector
 
 if __name__ == "__main__":
     module = TrendMicroModule()
     module.register(TrendMicroEmailSecurityConnector, "trend_micro_email_security")
-    module.register(TrendMicroVisionOneConnector, "trend_micro_vision_one")
+    module.register(TrendMicroVisionOneWorkbenchConnector, "trend_micro_vision_one_workbench")
     module.run()
