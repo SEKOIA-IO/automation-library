dff06ca1d4ce411056ce17beabdc7057c96870a7
lvoloshyn-sekoia <leonid.voloshyn@sekoia.io>
2025-01-22 10:55:33 +0200

    Fixes and improvements

--- MicrosoftOutlook/microsoft_outlook_modules/action_update_message.py
+++ MicrosoftOutlook/microsoft_outlook_modules/action_update_message.py
@@ -8,6 +8,10 @@ class UpdateMessageAction(MicrosoftGraphActionBase):
     def fill_non_empty(d: dict[str, Any]) -> dict[str, Any]:
         return {k: v for k, v in d.items() if v is not None}
 
+    @staticmethod
+    def generate_recipient(email: str) -> dict[str, Any]:
+        return {"emailAddress": {"name": email, "address": email}}
+
     def run(self, arguments: Any) -> Any:
         user_id_or_principal_name = arguments["user"]
         message_id = arguments["message_id"]
@@ -23,13 +27,13 @@ class UpdateMessageAction(MicrosoftGraphActionBase):
 
         payload: dict[str, Any] = self.fill_non_empty(
             {
-                "body": content,
-                "bccRecipients": bcc,
-                "ccRecipients": cc,
-                "sender": sender,
-                "from": mailbox_owner,
+                "body": {"content": content, "contentType": "text"} if content else None,  # plain text
+                "bccRecipients": [self.generate_recipient(item) for item in bcc] if bcc else None,
+                "ccRecipients": [self.generate_recipient(item) for item in cc] if cc else None,
+                "sender": self.generate_recipient(sender) if sender else None,
+                "from": self.generate_recipient(mailbox_owner) if mailbox_owner else None,
                 "subject": subject,
-                "toRecipients": recipients,
+                "toRecipients": [self.generate_recipient(item) for item in recipients] if recipients else None,
                 "importance": importance,
             }
         )
