c6506df206d53f66f0c21261ca7fac7c787e0115
TOUFIKI Zakarya <zakarya.toufiki@sekoia.io>
2025-02-07 11:24:03 +0100

    Add exception case to whois action

--- Whois/whois_module/whois_action.py
+++ Whois/whois_module/whois_action.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import whois
+from whois.parser import PywhoisError
 from sekoia_automation.action import Action
 
 
@@ -32,27 +33,30 @@ def time_list_tool(obj):
 
 class WhoisAction(Action):
     def run(self, arguments):
-        whois_result = whois.whois(arguments["query"])
-        return {
-            "Domain": {
-                "Name": str(list_tool(whois_result.domain_name, list, 0)),
-                "Whois": {
-                    "Domain": str(list_tool(whois_result.domain_name, list, 0)),
-                    "DomainStatus": whois_result.status,
-                    "DNSSec": str(whois_result.dnssec),
-                    "Raw": str(whois_result.text),
-                    "NameServers": whois_result.name_servers,
-                    "CreationDate": str(time_list_tool(whois_result.creation_date)),
-                    "UpdatedDate": str(time_list_tool(whois_result.updated_date)),
-                    "ExpirationDate": str(time_list_tool(whois_result.expiration_date)),
-                    "Registrar": {
-                        "Name": str(whois_result.registrar),
-                        "AbuseEmail": str(list_tool(whois_result.emails, list, 0)),
+        try:
+            whois_result = whois.whois(arguments["query"])
+            return {
+                "Domain": {
+                    "Name": str(list_tool(whois_result.domain_name, list, 0)),
+                    "Whois": {
+                        "Domain": str(list_tool(whois_result.domain_name, list, 0)),
+                        "DomainStatus": whois_result.status,
+                        "DNSSec": str(whois_result.dnssec),
+                        "Raw": str(whois_result.text),
+                        "NameServers": whois_result.name_servers,
+                        "CreationDate": str(time_list_tool(whois_result.creation_date)),
+                        "UpdatedDate": str(time_list_tool(whois_result.updated_date)),
+                        "ExpirationDate": str(time_list_tool(whois_result.expiration_date)),
+                        "Registrar": {
+                            "Name": str(whois_result.registrar),
+                            "AbuseEmail": str(list_tool(whois_result.emails, list, 0)),
+                        },
+                        "Registrant": {
+                            "Name": str(whois_result.get("name")),
+                            "Email": str(list_tool(whois_result.emails, list, 1)),
+                        },
                     },
-                    "Registrant": {
-                        "Name": str(whois_result.get("name")),
-                        "Email": str(list_tool(whois_result.emails, list, 1)),
-                    },
-                },
+                }
             }
-        }
+        except PywhoisError as e:
+            return {}
