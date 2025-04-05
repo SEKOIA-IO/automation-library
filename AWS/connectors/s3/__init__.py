28c4b02676555821cf5a1d8c75222b674553f34c
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2025-04-05 09:48:41 +0200

    refactor(AWS): change the criteria to stop the consumption of SQS messages

--- AWS/connectors/s3/__init__.py
+++ AWS/connectors/s3/__init__.py
@@ -132,6 +132,9 @@ class AbstractAwsS3QueuedConnector(AbstractAwsConnector, metaclass=ABCMeta):
                     except ValueError as e:
                         self.log_exception(e, message=f"Invalid JSON in message.\nInvalid message is: {message}")
 
+                if not message_records:
+                    continue_receiving = False
+
                 INCOMING_EVENTS.labels(intake_key=self.configuration.intake_key).inc(len(message_records))
                 for record in message_records:
                     try:
@@ -150,7 +153,6 @@ class AbstractAwsS3QueuedConnector(AbstractAwsConnector, metaclass=ABCMeta):
                                 records.append(event)
 
                                 if len(records) >= self.limit_of_events_to_push:
-                                    continue_receiving = False
                                     result += len(await self.push_data_to_intakes(events=records))
                                     records = []
 
