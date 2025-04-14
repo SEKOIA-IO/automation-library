4bf2a0738d3825218b2cf784719fc4430e54a9f5
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2025-04-14 10:24:18 +0200

    fix(Mimecast): fix mypy

--- Mimecast/mimecast_modules/models.py
+++ Mimecast/mimecast_modules/models.py
@@ -3,4 +3,4 @@ from pydantic import BaseModel, Field
 
 class MimecastModuleConfiguration(BaseModel):
     client_id: str = Field(..., description="Client ID")
-    client_secret: str = Field(secret=True, description="Client Secret")
+    client_secret: str = Field(secret=True, description="Client Secret")  # type: ignore
