17b3da9b3e9e12552a08ac0fb53dc5b03b2adb69
alexandreLavenant <alexandre.lavenant@sekoia.fr>
2025-06-02 11:15:48 +0200

    fix(xsiam): add the right name for docker parameter

--- PaloAltoXSIAM/main.py
+++ PaloAltoXSIAM/main.py
@@ -3,5 +3,5 @@ from xsiam.stix_to_xsiam import STIXToXSIAMAction
 
 if __name__ == "__main__":
     module = XsiamModule()
-    module.register(STIXToXSIAMAction, "stix-to-xsiam")
+    module.register(STIXToXSIAMAction, "stix_to_xsiam_action")
     module.run()
