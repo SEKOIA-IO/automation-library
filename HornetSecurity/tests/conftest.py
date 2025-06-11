8c951d697072550a57b6b6eb3d910883b383801d
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2025-06-11 18:26:28 +0200

    feat(HornetSecurity): add base connector

--- HornetSecurity/tests/conftest.py
+++ HornetSecurity/tests/conftest.py
@@ -4,6 +4,9 @@ from tempfile import mkdtemp
 import pytest
 from sekoia_automation import constants
 
+from hornetsecurity_modules import HornetsecurityModule
+from hornetsecurity_modules.models import HornetsecurityModuleConfiguration
+
 
 @pytest.fixture
 def data_storage():
@@ -14,3 +17,16 @@ def data_storage():
 
     rmtree(constants.DATA_STORAGE)
     constants.DATA_STORAGE = original_storage
+
+
+@pytest.fixture
+def module():
+    """
+    Fixture to create an instance of HornetsecurityModule for testing.
+    """
+    module = HornetsecurityModule()
+    module.configuration = HornetsecurityModuleConfiguration(
+        api_token="my_api_token",
+        api_url="https://cp.hornetsecurity.com/api/v0",
+    )
+    return module
---
+++ HornetSecurity/tests/test_connector_base.py
@@ -0,0 +1,123 @@
+from collections.abc import Generator
+from datetime import datetime, timedelta, timezone
+from unittest.mock import Mock, MagicMock
+
+import pytest
+
+from hornetsecurity_modules.connector_base import (
+    BaseConnector,
+    BaseConnectorConfiguration,
+)
+
+
+class DummyConnector(BaseConnector):
+    ID_FIELD = "id"
+    events: list[list[dict]] = []
+
+    def set_events(self, events: list[list[dict]]) -> None:
+        """
+        Set the events to be returned by the connector.
+        """
+        self.events = events
+
+    def _fetch_events(self, from_date: datetime, to_date: datetime) -> Generator[list[dict], None, None]:
+        """
+        Fetch events from the dummy connector.
+        """
+        for event in self.events:
+            yield event
+        yield []
+
+
+@pytest.fixture
+def connector_base(module, data_storage):
+    connector = DummyConnector(module=module, data_path=data_storage)
+    connector.configuration = BaseConnectorConfiguration(
+        frequency=300,
+        chunk_size=2,
+        timedelta=0,
+        ratelimit_per_second=20,
+        intake_key="fake_intake_key",
+    )
+    connector.log = MagicMock()
+    connector.log_exception = MagicMock()
+    connector.push_events_to_intakes = MagicMock()
+    connector.time_stepper.ranges = Mock(
+        side_effect=[
+            [
+                (
+                    datetime.now(timezone.utc) - timedelta(minutes=5),
+                    datetime.now(timezone.utc),
+                )
+            ]
+        ]
+    )
+    return connector
+
+
+def test_connector_base_fetch_events_no_events(connector_base):
+    """
+    Test the fetch_events method of ConnectorBase when no events are returned.
+    """
+    connector_base.set_events([])
+
+    with pytest.raises(StopIteration):
+        next(connector_base.fetch_events())
+
+
+def test_connector_base_fetch_events(connector_base):
+    """
+    Test the fetch_events method of ConnectorBase.
+    """
+    fetched_events = [
+        {
+            "id": "event1",
+            "date": "2023-10-01T12:00:00Z",
+            "direction": "Incoming",
+            "subject": "Test Email 1",
+        },
+        {
+            "id": "event2",
+            "date": "2023-10-01T12:05:00Z",
+            "direction": "Outgoing",
+            "subject": "Test Email 2",
+        },
+        {
+            "id": "event1",
+            "date": "2023-10-01T12:05:00Z",
+            "direction": "Incoming",
+            "subject": "Test Email 2",
+        },
+    ]
+    connector_base.set_events([fetched_events])
+
+    events = next(connector_base.fetch_events())
+    assert len(events) == 2
+    assert events[0]["id"] == "event1"
+    assert events[1]["id"] == "event2"
+
+
+def test_connector_base_next_batch(connector_base):
+    """
+    Test the next_batch method of ConnectorBase.
+    """
+    fetched_events = [
+        {
+            "id": "event1",
+            "date": "2023-10-01T12:00:00Z",
+            "direction": "Incoming",
+            "subject": "Test Email 1",
+        },
+        {
+            "id": "event2",
+            "date": "2023-10-01T12:05:00Z",
+            "direction": "Outgoing",
+            "subject": "Test Email 2",
+        },
+    ]
+    connector_base.set_events([fetched_events])
+
+    connector_base.next_batch()
+    assert connector_base.push_events_to_intakes.call_count == 1
+    calls = [call.kwargs["events"] for call in connector_base.push_events_to_intakes.call_args_list]
+    assert len(calls[0]) == 2
