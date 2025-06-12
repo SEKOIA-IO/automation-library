369a6df8ffb6b4943ecf393eee5be744602ffade
vg-svitla <v.huriev@svitla.com>
2025-06-12 16:42:56 +0400

    Feature: WatchGuard EPDR

---
+++ WatchGuard/tests/test_epdr_connector.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+from unittest.mock import AsyncMock
+
+import aiohttp
+import pytest
+from aioresponses import aioresponses
+
+from watchguard import WatchGuardModule
+from watchguard.client.security_event import SecurityEvent
+from watchguard.epdr_connector import WatchGuardEpdrConnector
+
+from .helpers import aioresponses_callback
+
+
+@pytest.fixture
+def epdr_connector(module: WatchGuardModule, mock_push_data_to_intakes: AsyncMock) -> WatchGuardEpdrConnector:
+    """
+    Create an instance of the WatchGuardEpdrConnector with the provided module.
+
+    Args:
+        module: The WatchGuard module instance.
+        mock_push_data_to_intakes: AsyncMock for pushing data to intakes.
+
+    Returns:
+        WatchGuardEpdrConnector: An instance of the WatchGuardEpdrConnector.
+    """
+    connector = WatchGuardEpdrConnector(module=module)
+    connector.push_data_to_intakes = mock_push_data_to_intakes
+
+    return connector
+
+
+@pytest.mark.asyncio
+async def test_epdr_connector(epdr_connector: WatchGuardEpdrConnector, session_faker) -> None:
+    response_data = {}
+    for security_event in SecurityEvent:
+        response_data[security_event] = {
+            **session_faker.pydict(value_types=[str, int, float, bool, list, dict]),
+            "date": datetime.now().isoformat(),
+        }
+
+    auth_token = session_faker.word()
+    module_config = epdr_connector.module.configuration
+    with aioresponses() as mocked_responses:
+        auth_token_url = "{}/oauth/token".format(module_config.base_url)
+        mocked_responses.post(
+            auth_token_url,
+            callback=aioresponses_callback(
+                {
+                    "access_token": auth_token,
+                },
+                auth=aiohttp.BasicAuth(module_config.username, module_config.password),
+                status=200,
+            ),
+        )
+
+        for security_event in SecurityEvent:
+            data = [
+                {
+                    **session_faker.pydict(value_types=[str, int]),
+                    "date": datetime.now().isoformat(),
+                }
+                for _ in range(session_faker.random_int(min=1, max=3000))
+            ]
+
+            response_data[security_event] = data
+
+            data_url = "{0}/api/v1/accounts/{1}/securityevents/{2}/export/{3}".format(
+                module_config.base_url, module_config.account_id, security_event.value, 1
+            )
+
+            mocked_responses.get(
+                data_url,
+                callback=aioresponses_callback(
+                    {"data": data},
+                    headers={
+                        "WatchGuard-API-Key": module_config.application_key,
+                        "Authorization": f"Bearer {auth_token}",
+                    },
+                    status=200,
+                ),
+            )
+
+        result = await epdr_connector.get_watchguard_events()
+    assert result == sum([len(value) for _, value in response_data.items()])
---
+++ WatchGuard/watchguard/client/http_client.py
@@ -0,0 +1,169 @@
+from contextlib import asynccontextmanager
+from typing import Any, AsyncGenerator, Awaitable, Callable
+
+import aiohttp
+from aiohttp import ClientSession
+from aiolimiter import AsyncLimiter
+from pydantic.v1 import BaseModel
+
+from watchguard.client.errors import WatchGuardAuthError, WatchGuardError
+from watchguard.client.security_event import SecurityEvent
+
+
+class WatchGuardClientConfig(BaseModel):
+    """
+    Configuration for the WatchGuard client.
+    """
+
+    username: str
+    password: str
+    account_id: str
+    application_key: str
+    base_url: str = "https://api.deu.cloud.watchguard.com"
+
+
+class WatchGuardClient(object):
+    _session: ClientSession | None = None
+    _rate_limiter_daily: AsyncLimiter | None = None
+    _rate_limiter_second: AsyncLimiter | None = None
+    _auth_token: str | None = None
+
+    def __init__(
+        self,
+        config: WatchGuardClientConfig,
+    ) -> None:
+        """
+        Initialize the WatchGuardClient with the provided credentials and base URL.
+
+        Args:
+            config: WatchGuardClientConfig
+        """
+        self.config = config
+        if not self._rate_limiter_second:
+            self.set_rate_limiter_second(AsyncLimiter(max_rate=500, time_period=1))  # 500 requests per second
+
+        if not self._rate_limiter_daily:
+            self.set_rate_limiter_daily(AsyncLimiter(max_rate=200_000, time_period=86400))  # 200,000 requests per day
+
+    @classmethod
+    def set_rate_limiter_daily(cls, rate_limiter: AsyncLimiter) -> None:
+        """
+        Set rate limiter.
+
+        Args:
+            rate_limiter:
+        """
+        cls._rate_limiter_daily = rate_limiter
+
+    @classmethod
+    def set_rate_limiter_second(cls, rate_limiter: AsyncLimiter) -> None:
+        """
+        Set rate limiter.
+
+        Args:
+            rate_limiter:
+        """
+        cls._rate_limiter_second = rate_limiter
+
+    @classmethod
+    @asynccontextmanager
+    async def session(cls) -> AsyncGenerator[ClientSession, None]:
+        """
+        Get configured session with rate limiter.
+
+        Returns:
+            AsyncGenerator[ClientSession, None]:
+        """
+        if cls._session is None:
+            cls._session = ClientSession()
+
+        if cls._rate_limiter_daily and cls._rate_limiter_second:
+            async with cls._rate_limiter_daily:
+                async with cls._rate_limiter_second:
+                    yield cls._session
+        else:
+            yield cls._session
+
+    async def fetch_auth_token(self) -> str:
+        """
+        Fetch the authentication token from the WatchGuard API.
+
+        Returns:
+            str: The authentication token.
+        """
+        async with self.session() as session:
+            response = await session.post(
+                f"{self.config.base_url}/oauth/token",
+                auth=aiohttp.BasicAuth(self.config.username, self.config.password),
+            )
+
+            if response.status != 200:
+                raise await WatchGuardError.from_response(response)
+
+            data = await response.json()
+
+            result: str | None = data.get("access_token")
+            if not result:
+                raise ValueError("WatchGuard auth response does not contain a access token. Response: {}".format(data))
+
+        return result
+
+    async def _call_with_auth(self, func: Callable[[str], Awaitable[dict[str, Any]]]) -> dict[str, Any]:
+        """
+        Call a function with authentication.
+
+        Args:
+            func: A callable that takes the base URL and returns a dictionary.
+
+        Returns:
+            dict[str, Any]: The result of the function call.
+        """
+        if not self._auth_token:
+            self._auth_token = await self.fetch_auth_token()
+
+        try:
+            return await func(self._auth_token)
+        except WatchGuardAuthError:
+            # If authentication fails, fetch a new token and retry
+            self._auth_token = await self.fetch_auth_token()
+
+            return await func(self._auth_token)
+
+    async def fetch_data(self, security_event: SecurityEvent, period: int = 1) -> AsyncGenerator[dict[str, Any], None]:
+        """
+        Fetch data from the WatchGuard API.
+
+        Args:
+            security_event: SecurityEvent
+            period: int
+
+        Yields:
+            dict[str, Any]: The fetched data.
+        """
+        # https://www.watchguard.com/help/docs/API/Content/en-US/endpoint_security/WES_endpoint_security/v1/WES_endpoint_security.html#Retrieve_Security_Events_for_Devices_..184
+        if period not in [1, 7]:
+            raise ValueError("Period must be either 1 or 7 days.")
+
+        async with self.session() as session:
+
+            async def fetch_func(auth_token: str) -> dict[str, Any]:
+                headers = {"WatchGuard-API-Key": self.config.application_key, "Authorization": f"Bearer {auth_token}"}
+
+                response = await session.get(
+                    "{0}/api/v1/accounts/{1}/securityevents/{2}/export/{3}".format(
+                        self.config.base_url, self.config.account_id, security_event.value, period
+                    ),
+                    headers=headers,
+                )
+
+                if response.status != 200:
+                    raise await WatchGuardError.from_response(response)
+
+                result: dict[str, Any] = await response.json()
+
+                return result
+
+            data = await self._call_with_auth(fetch_func)
+
+            for item in data.get("data", []):
+                yield item
