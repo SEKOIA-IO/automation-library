440c5e0259042a39b2c734bf6054c8818072b5d7
vg-svitla <v.huriev@svitla.com>
2025-07-17 15:14:31 +0400

    Fix comments

--- Nozomi/nozomi_networks/__init__.py
+++ Nozomi/nozomi_networks/__init__.py
@@ -5,10 +5,7 @@ from sekoia_automation.module import Module
 class NozomiConfiguration(BaseModel):
     key_name: str = Field(..., description="Nozomi Key Name")
     key_token: str = Field(..., secret=True, description="Nozomi Key Token")
-    base_url: str = Field(
-        "https://vantagetechalliancesinstance.customers.us1.vantage.nozominetworks.io",
-        description="Base URL for Nozomi Vantage API",
-    )
+    base_url: str = Field(..., description="Base URL for Nozomi Vantage API")
 
 
 class NozomiModule(Module):
