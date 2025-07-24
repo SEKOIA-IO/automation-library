37575c05f441f9d49aca256ef7b364e3dc397b3e
TOUFIKI Zakarya <zakarya.toufiki@sekoia.io>
2025-07-24 14:44:55 +0200

    Add accout validator to harfanglab module

--- HarfangLab/main.py
+++ HarfangLab/main.py
@@ -1,5 +1,6 @@
 from sekoia_automation.module import Module
 
+from harfanglab.account_validator import HarfanglabAccountValidator
 from harfanglab.asset_connector.device_assets import HarfanglabAssetConnector
 from harfanglab.download_file_from_endpoint import DownloadFileFromEndpointAction
 from harfanglab.endpoint_actions import (
@@ -17,6 +18,7 @@ from harfanglab.threat_actions import AddCommentToThreat, UpdateThreatStatus
 
 if __name__ == "__main__":
     module = Module()
+    module.register_account_validator(HarfanglabAccountValidator)
     module.register(HarfanglabAssetConnector, "harfanglab_asset_connector")
     module.register(GetProcessListAction, "harfanglab_get_process_list")
     module.register(GetPipeListAction, "harfanglab_get_pipe_list")
---
+++ HarfangLab/tests/test_account_validator.py
@@ -0,0 +1,51 @@
+import pytest
+from unittest.mock import Mock
+
+from sekoia_automation.module import Module
+
+from harfanglab.account_validator import HarfanglabAccountValidator
+
+
+@pytest.fixture
+def harfanglab_account_validator():
+
+    module = Module()
+    module.configuration = {
+        "url": "http://example.com",
+        "api_token": "fake_api_key",
+    }
+    harfanglab_validator = HarfanglabAccountValidator(module=module)
+
+    harfanglab_validator.log = Mock()
+    harfanglab_validator.log_exception = Mock()
+
+    yield harfanglab_validator
+
+
+
+def test_validate_success(harfanglab_account_validator, requests_mock):
+    requests_mock.get(
+        "https://example.com/api/auth/users/me",
+        json={"username": "test_user"},
+        status_code=200,
+    )
+
+    assert harfanglab_account_validator.validate() is True
+
+def test_validate_failure(harfanglab_account_validator, requests_mock):
+    requests_mock.get(
+        "https://example.com/api/auth/users/me",
+        json={"detail": "Invalid credentials"},
+        status_code=401,
+    )
+
+    assert harfanglab_account_validator.validate() is False
+
+def test_validate_unexpected_status(harfanglab_account_validator, requests_mock):
+    requests_mock.get(
+        "https://example.com/api/auth/users/me",
+        json={"detail": "Unexpected error"},
+        status_code=500,
+    )
+
+    assert harfanglab_account_validator.validate() is False
