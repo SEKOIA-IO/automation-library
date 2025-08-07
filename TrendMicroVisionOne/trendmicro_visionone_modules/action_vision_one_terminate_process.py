e661c1dc27af6e64351d825d06d78b59054a9e7d
lvoloshyn-sekoia <leonid.voloshyn@sekoia.io>
2025-08-07 14:56:11 +0300

    Fixes and improvements

--- TrendMicroVisionOne/trendmicro_visionone_modules/action_vision_one_terminate_process.py
+++ TrendMicroVisionOne/trendmicro_visionone_modules/action_vision_one_terminate_process.py
@@ -5,8 +5,7 @@ from .action_vision_one_base import TrendMicroVisionOneBaseAction
 
 class TerminateProcessAction(TrendMicroVisionOneBaseAction):
     def run(self, arguments: Any) -> Any:
-        agent_guids: list[str] = arguments["agent_guids"]
-        process_id: int = arguments["process_id"]
+        agent_guids_process_ids: list[dict] = arguments["agent_guids_process_ids"]
 
         file_sha1: str | None = arguments.get("file_sha1")
         file_name: str | None = arguments.get("file_name")
@@ -16,8 +15,8 @@ class TerminateProcessAction(TrendMicroVisionOneBaseAction):
         url = f"{base_url}/v3.0/response/endpoints/terminateProcess"
 
         payload: list[dict[str, Any]] = []
-        for agent_guid in agent_guids:
-            item = {"agentGuid": agent_guid, "processId": process_id}
+        for group in agent_guids_process_ids:
+            item = {"agentGuid": group["agent_guid"], "processId": group["process_id"]}
             if description:
                 item["description"] = description
 
