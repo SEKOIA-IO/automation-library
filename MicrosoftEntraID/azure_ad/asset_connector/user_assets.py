71a6a159fa0df25e6182c871248ded765562920c
mchupeau-sk <mehidine.chupeau@sekoia.io>
2025-08-11 12:46:56 +0000

    init commit

--- MicrosoftEntraID/azure_ad/asset_connector/user_assets.py
+++ MicrosoftEntraID/azure_ad/asset_connector/user_assets.py
@@ -1,46 +1,58 @@
+import asyncio
 from functools import cached_property
 from collections.abc import Generator
-from typing import Any, Union
-from urllib.parse import urljoin
 
-from dateutil.parser import isoparse
-from datetime import timedelta, datetime
 from azure.identity.aio import ClientSecretCredential  # async credentials only
 from kiota_authentication_azure.azure_identity_authentication_provider import AzureIdentityAuthenticationProvider
 from msgraph import GraphRequestAdapter, GraphServiceClient
 
-from sekoia_automation.checkpoint import CheckpointDatetime
 from sekoia_automation.asset_connector import AssetConnector
 from sekoia_automation.asset_connector.models.ocsf.user import (
     UserOCSFModel,
-    User,
-    Metadata,
+    User as UserOCSF,
+    Group as UserOCSFGroup,
 )
+from sekoia_automation.asset_connector.models.ocsf.base import (
+    Metadata,
+    Product,
+)
+from sekoia_automation.storage import PersistentJSON
 
-from .helpers import handle_uri
+from kiota_abstractions.base_request_configuration import RequestConfiguration
+from msgraph.generated.users.users_request_builder import UsersRequestBuilder
+from msgraph.generated.models.software_oath_authentication_method import SoftwareOathAuthenticationMethod
+from msgraph.generated.models.microsoft_authenticator_authentication_method import (
+    MicrosoftAuthenticatorAuthenticationMethod,
+)
+from msgraph.generated.models.phone_authentication_method import PhoneAuthenticationMethod
+from msgraph.generated.models.group import Group
+from msgraph.generated.models.user import User
 
 
 class EntraIDAssetConnector(AssetConnector):
 
-    USERS_ENDPOINT: str = "/users"
-    GROUPS_ENDPOINT: str = "/groups"
-    PRODUCT_NAME: str = "Entra ID"
+    PRODUCT_NAME = "Microsoft Entra ID"
+    PRODUCT_VERSION = "1.0"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.context = PersistentJSON("context.json", self._data_path)
+        self._client: GraphServiceClient | None = None
 
-        self.cursor = CheckpointDatetime(
-            path=self._data_path, start_at=timedelta(hours=24), ignore_older_than=timedelta(days=7)
-        )
-        self.from_date = self.cursor.offset
+    @property
+    def most_recent_date_seen(self) -> str | None:
+        with self.context as cache:
+            most_recent_date_seen = cache.get("most_recent_date_seen", None)
+
+            return most_recent_date_seen
 
     @cached_property
     def client(self) -> GraphServiceClient:
         if self._client is None:
             credentials = ClientSecretCredential(
-                tenant_id=self.module.configuration.tenant_id,
-                client_id=self.module.configuration.client_id,
-                client_secret=self.module.configuration.client_secret,
+                tenant_id=self.module.configuration["tenant_id"],
+                client_id=self.module.configuration["client_id"],
+                client_secret=self.module.configuration["client_secret"],
             )
             auth_provider = AzureIdentityAuthenticationProvider(credentials)
             adapter = GraphRequestAdapter(auth_provider)
@@ -48,146 +60,131 @@ class EntraIDAssetConnector(AssetConnector):
 
         return self._client
 
-    @cached_property
-    def base_url(self) -> str:
-        """
-        Get the base URL for the asset connector.
+    def map_fields(self, user: User, has_mfa: bool, groups: list[UserOCSFGroup]) -> UserOCSFModel:
+        """Map fields from UserCollectionResponse to UserOCSFModel.
+        Args:
+            user (UserCollectionResponse): The user data from Microsoft Graph API.
 
         Returns:
-            str: The base URL.
+            UserOCSFModel: The mapped OCSF model.
         """
-        return handle_uri(self.module.configuration["url"])
-
-    @cached_property
-    def limit(self) -> int:
-        """
-        Get the limit for the number of assets to retrieve.
-
-        Returns:
-            int: The limit.
-        """
-        return 1000
-
-    @staticmethod
-    def extract_timestamp( asset: dict[str, Any]) -> datetime:
-        """
-        Extract the timestamp from the asset data and transform it to datetime.
-
-        Returns:
-            datetime: The extracted timestamp as a datetime object.
-        """
-        return isoparse(asset["firstseen"])
-
-    @staticmethod
-    def extract_os_type(os_type: str | None) -> str:
-        """
-        Extract the OS type from the asset data and map it.
-
-        returns:
-            str: The OS type of asset.
-        """
-        os_type_list = [member.name for member in OSTypeStr]
-        os_type = os_type.strip().upper() if os_type else ""
-
-        if not os_type:
-            return "UNKNOWN"
-
-        if os_type and os_type not in os_type_list:
-            return "OTHER"
-
-        return os_type
-
-    def map_fields(self, asset: dict[str, Any]) -> DeviceOCSFModel:
-        """
-        Map the fields from the asset data to the OCSF Device model.
-
-        return:
-            DeviceOCSFModel: The mapped OCSF Device model.
-        """
-        product = Product(name=self.PRODUCT_NAME, version="24.12")
-        metadata = Metadata(product=product, version="1.5.0")
-        os_type = self.extract_os_type(asset.get("ostype"))
-        os_obj = OperatingSystem(name=asset.get("osproducttype"), type=OSTypeStr[os_type], type_id=OSTypeId[os_type])
-        device = Device(
-            type_id=DeviceTypeId.DESKTOP,
-            type=DeviceTypeStr.DESKTOP,
-            uid=asset["id"],
-            os=os_obj,
-            hostname=asset["hostname"],
+        product = Product(
+            name=self.PRODUCT_NAME,
+            version=self.PRODUCT_VERSION,
         )
-        return DeviceOCSFModel(
+        metadata = Metadata(product=product, version="1.0")
+        user_data = UserOCSF(
+            has_mfa=has_mfa,
+            name=user.user_principal_name or "Unknown",
+            uid=0,  # Placeholder, need to change to a unique identifier
+            groups=groups,
+            full_name=user.display_name or "Unknown",
+            email_addr=user.mail or "Unknown",
+        )
+        user_ocsf_model = UserOCSFModel(
             activity_id=2,
             activity_name="Collect",
             category_name="Discovery",
             category_uid=5,
-            class_name="Asset",
-            class_uid=5001,
-            type_name="Software Inventory Info: Collect",
-            type_uid=500102,
-            time=self.extract_timestamp(asset).timestamp(),
+            class_name="User Inventory Info",
+            class_uid=5003,
+            type_uid=500302,
+            severity="Informational",
+            severity_id=1,
+            time=0.0,  # Placeholder, adjust as needed
             metadata=metadata,
-            device=device,
+            user=user_data,
+            type_name="User Inventory",
+        )
+        return user_ocsf_model
+
+    async def fetch_user_groups(self, user_id: str) -> list[UserOCSFGroup]:
+        """
+        Fetch user groups from Microsoft Entra ID.
+        """
+        groups: list[UserOCSFGroup] = []
+        user_groups = await self.client.users.by_user_id(user_id).member_of.get()
+
+        if user_groups:
+            for group in user_groups.value:
+                if isinstance(group, Group):
+                    groups.append(UserOCSFGroup(name=group.display_name, uid=group.id))
+
+        ## Implement if there is more than one page of results
+        while user_groups is not None and user_groups.odata_next_link is not None:
+            user_groups = (
+                await self.client.users.by_user_id(user_id).member_of.with_url(user_groups.odata_next_link).get()
+            )
+            if user_groups:
+                for group in user_groups.value:
+                    if isinstance(group, Group):
+                        groups.append(UserOCSFGroup(name=group.display_name, uid=group.id))
+
+        return groups
+
+    async def fetch_user_mfa(self, user_id: str) -> bool:
+        """
+        Fetch MFA status of the user.
+        """
+        user_mfa = await self.client.users.by_user_id(user_id).authentication.methods.get()
+        has_mfa = False
+        for method in user_mfa.value:
+            if (
+                isinstance(method, MicrosoftAuthenticatorAuthenticationMethod)
+                or isinstance(method, SoftwareOathAuthenticationMethod)
+                or isinstance(method, PhoneAuthenticationMethod)
+            ):
+                has_mfa = True
+                break
+        return has_mfa
+
+    async def fetch_user(self, user: User) -> UserOCSFModel:
+        """
+        Fetch user details and map to UserOCSFModel.
+        """
+        user_mfa = await self.fetch_user_mfa(user.id)
+        user_groups = await self.fetch_user_groups(user.id)
+        return self.map_fields(user, user_mfa, user_groups)
+
+    async def fetch_new_users(self, last_run_date: str | None = None) -> list[UserOCSFModel]:
+        """
+        Fetch new users from Microsoft Entra ID.
+        If last_run_date is provided, only fetch users created after that date.
+        """
+        new_users: list[UserOCSFModel] = []
+        query_params = UsersRequestBuilder.UsersRequestBuilderGetQueryParameters(
+            select=["id", "displayName", "mail", "identities", "createdDateTime", "userPrincipalName", "mailNickname"],
         )
 
-    def __fetch_devices(self, from_date: datetime) -> Generator[list[dict[str, Any]], None, None]:
-        devices_url = urljoin(self.base_url, self.AGENT_ENDPOINT)
-        offset = 0
-        firstseen_param: str = from_date.isoformat()
+        request_configuration = RequestConfiguration(
+            query_parameters=query_params,
+        )
+        users = await self.client.users.get(request_configuration=request_configuration)
 
-        params: dict[str, Union[str, int]] = {
-            "ordering": self.DEVICE_ORDERING_FIELD,
-            "firstseen": firstseen_param,
-            "limit": self.limit,
-            "offset": offset,
-        }
+        if users:
+            for user in users.value:
+                ## Fetch MFA status of the user
+                test = await self.fetch_user(user)
+                new_users.append(test)
+                # yield test
 
-        device_response = self.client.get(devices_url, params=params)
-        device_response.raise_for_status()
+        ## Implement if there is more than one page of results
 
-        while self.running:
-            devices = device_response.json()
+        while users is not None and users.odata_next_link is not None:
+            users = await self.client.users.with_url(users.odata_next_link).get(
+                request_configuration=request_configuration
+            )
+            if users:
+                for user in users.value:
+                    test = await self.fetch_user(user)
+                    new_users.append(test)
+                    # yield self.map_fields(user)
 
-            # Check if there are no devices or if the count is zero
-            # This is to handle the case where there are no devices returned
-            if not devices or devices.get("count") == 0:
-                return
+        return new_users
 
-            yield devices.get("results", [])
-
-            next_page = devices.get("next")
-
-            # If there is no next page, break the loop
-            if not next_page:
-                return
-
-            # Update the offset for the next request
-            offset += self.limit
-            params["offset"] = offset
-
-            device_response = self.client.get(next_page, params=params)
-            device_response.raise_for_status()
-
-    def next_list_devices(self) -> Generator[list[dict[str, Any]], None, None]:
-        most_recent_date_seen = self.from_date
-
-        for devices in self.__fetch_devices(from_date=most_recent_date_seen):
-            if devices:
-                last_event = max(devices, key=self.extract_timestamp)
-                last_event_datetime = self.extract_timestamp(last_event)
-
-                # Update the most recent date seen if the last event is more recent
-                if last_event_datetime > most_recent_date_seen:
-                    most_recent_date_seen = last_event_datetime + timedelta(microseconds=1)
-
-                yield devices
-
-        # Update the cursor with the most recent date seen
-        if most_recent_date_seen > self.from_date:
-            self.cursor.offset = most_recent_date_seen
-            self.from_date = most_recent_date_seen
-
-    def get_assets(self) -> Generator[DeviceOCSFModel, None, None]:
-        for devices in self.next_list_devices():
-            for device in devices:
-                mapped_device: DeviceOCSFModel = self.map_fields(device)
-                yield mapped_device
+    def get_assets(self) -> Generator[UserOCSFModel, None, None]:
+        ### Fetch users from Microsoft Graph API
+        # last_run_date = self.most_recent_date_seen
+        new_users = asyncio.run(self.fetch_new_users())
+        for user in new_users:
+            yield user
