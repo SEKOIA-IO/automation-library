535231ffe16ab2a393e807443b9c7b661c93d31d
TOUFIKI Zakarya <zakarya.toufiki@sekoia.io>
2025-08-28 17:43:11 +0200

    chore(tenable_conf): update tenable conf

--- Tenable/tenable_conn/__init__.py
+++ Tenable/tenable_conn/__init__.py
@@ -4,7 +4,7 @@ from sekoia_automation.module import Module
 
 class TenableModuleConfiguration(BaseModel):
     base_url: str = Field(..., description="Base URL")
-    access_token: str = Field(..., description="Access Token")
+    access_key: str = Field(..., description="Access key")
     secret_key: str = Field(..., description="Secret Key", secret=True)
 
 
