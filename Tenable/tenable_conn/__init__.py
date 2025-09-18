17751478f315319dabf73fb44cb3e1a166863d11
TOUFIKI Zakarya <zakarya.toufiki@sekoia.io>
2025-09-18 14:06:54 +0200

    fix(mypy): fiw mypy

--- Tenable/tenable_conn/__init__.py
+++ Tenable/tenable_conn/__init__.py
@@ -5,7 +5,7 @@ from sekoia_automation.module import Module
 class TenableModuleConfiguration(BaseModel):
     base_url: str = Field(..., description="Base URL")
     access_key: str = Field(..., description="Access key")
-    secret_key: str = Field(..., description="Secret Key", secret=True)
+    secret_key: str = Field(..., description="Secret Key")
 
 
 class TenableModule(Module):
