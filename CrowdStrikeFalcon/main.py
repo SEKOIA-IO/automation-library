6b66f350d59cef90cc6e211db22fd09e3fbd76c1
TOUFIKI Zakarya <zakarya.toufiki@sekoia.io>
2025-09-25 07:51:34 +0200

    Crowdstrike user asset connector : Add it for the first time

--- CrowdStrikeFalcon/main.py
+++ CrowdStrikeFalcon/main.py
@@ -1,3 +1,4 @@
+from crowdstrike_falcon.account_validator import CrowdstrikeFalconAccountValidator
 from crowdstrike_falcon import CrowdStrikeFalconModule
 from crowdstrike_falcon.alert_actions import CrowdstrikeActionCommentAlert, CrowdstrikeActionUpdateAlertStatus
 from crowdstrike_falcon.custom_iocs import (
@@ -8,9 +9,12 @@ from crowdstrike_falcon.custom_iocs import (
 )
 from crowdstrike_falcon.event_stream_trigger import EventStreamTrigger
 from crowdstrike_falcon.host_actions import CrowdstrikeActionDeIsolateHosts, CrowdstrikeActionIsolateHosts
+from crowdstrike_falcon.asset_connectors.user_assets import CrowdstrikeUserAssetConnector
 
 if __name__ == "__main__":
     module = CrowdStrikeFalconModule()
+    module.register_account_validator(CrowdstrikeFalconAccountValidator)
+    module.register(CrowdstrikeUserAssetConnector, "crowdstrike_falcon_user_asset_connector")
     module.register(EventStreamTrigger, "event_stream_trigger")
     module.register(CrowdstrikeActionPushIOCsBlock, "push_iocs_block")
     module.register(CrowdstrikeActionPushIOCsDetect, "push_iocs_detect")
