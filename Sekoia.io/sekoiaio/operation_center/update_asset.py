97dbfd868d55955e7e4b3ad3f9c235b90192f3c2
lvoloshyn-sekoia <leonid.voloshyn@sekoia.io>
2025-09-25 18:35:22 +0300

    Small refactoring

--- Sekoia.io/sekoiaio/operation_center/update_asset.py
+++ Sekoia.io/sekoiaio/operation_center/update_asset.py
@@ -6,6 +6,18 @@ from sekoia_automation.action import Action
 
 
 class UpdateAsset(Action):
+    ALLOWED_FIELDS = [
+        "entity_uuid",
+        "name",
+        "description",
+        "type",
+        "category",
+        "criticality",
+        "props",
+        "atoms",
+        "tags",
+    ]
+
     def url(self, path: str) -> str:
         return urljoin(self.module.configuration["base_url"], "api/v2/asset-management/assets/", path)
 
@@ -15,7 +27,7 @@ class UpdateAsset(Action):
         return {"Authorization": f"Bearer {api_key}"}
 
     def perform_request(self, asset_uuid: str, payload: dict) -> Any:
-        result = requests.put(self.url(asset_uuid), headers=self.headers, json=payload)
+        result = requests.put(self.url(asset_uuid), headers=self.headers, json=payload, timeout=60)
 
         if not result.ok:
             self.error(f"Could not fetch asset {asset_uuid}, status code: {result.status_code}")
@@ -26,51 +38,14 @@ class UpdateAsset(Action):
     def run(self, arguments: Any) -> Any:
         asset_uuid = arguments["uuid"]
 
-        payload = {}
-        entity_uuid = arguments.get("entity_uuid")
-        if entity_uuid:
-            payload["entity_uuid"] = entity_uuid
-
-        name = arguments.get("name")
-        if name:
-            payload["name"] = name
-
-        description = arguments.get("description")
-        if description:
-            payload["description"] = description
-
-        asset_type = arguments.get("type")
-        if asset_type:
-            payload["type"] = asset_type
-
-        category = arguments.get("category")
-        if category:
-            payload["category"] = category
-
-        criticality = arguments.get("criticality")
-        if criticality:
-            payload["criticality"] = criticality
-
-        props = arguments.get("props")
-        if props:
-            payload["props"] = props
-
-        atoms = arguments.get("atoms")
-        if atoms:
-            payload["atoms"] = atoms
-
-        tags = arguments.get("tags")
-        if tags:
-            payload["tags"] = tags
+        payload = {k: arguments[k] for k in self.ALLOWED_FIELDS if arguments.get(k)}
 
         revoked = arguments.get("revoked")
-        if revoked:
+        if revoked is not None:
             payload["revoked"] = revoked
 
         reviewed = arguments.get("reviewed")
-        if reviewed:
+        if reviewed is not None:
             payload["reviewed"] = reviewed
 
-        response = self.perform_request(asset_uuid=asset_uuid, payload=payload)
-
-        return response
+        return self.perform_request(asset_uuid=asset_uuid, payload=payload)
