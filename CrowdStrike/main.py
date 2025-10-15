7168a7c754b75608ecc82a3cdca89bbd903dc63c
vg-svitla <v.huriev@svitla.com>
2025-10-15 20:04:19 +0400

    Cleanup

--- CrowdStrike/main.py
+++ CrowdStrike/main.py
@@ -2,8 +2,6 @@
 An entrypoint to work with CrowdStrike telemetry events.
 """
 
-from connectors import AwsModule
-
 from crowdstrike_telemetry import CrowdStrikeTelemetryModule
 from crowdstrike_telemetry.pull_telemetry_events import CrowdStrikeTelemetryConnector
 
