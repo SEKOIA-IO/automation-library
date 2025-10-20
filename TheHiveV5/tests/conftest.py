28e7f3cc29e1d17f951f285804f0cf887b158c61
CharlesLR-sekoia <charles.le.reun@sekoia.io>
2025-10-20 16:27:25 +0200

    first batch of fixes following review. More to come.

---
+++ TheHiveV5/tests/conftest.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from shutil import rmtree
+from tempfile import mkdtemp
+
+import pytest
+from sekoia_automation import constants
+from sekoia_automation.module import Module
+
+
+@pytest.fixture
+def data_storage():
+    original_storage = constants.DATA_STORAGE
+    constants.DATA_STORAGE = mkdtemp()
+
+    yield Path(constants.DATA_STORAGE)
+
+    rmtree(constants.DATA_STORAGE)
+    constants.DATA_STORAGE = original_storage
+
+
+@pytest.fixture
+def module():
+    yield Module()
