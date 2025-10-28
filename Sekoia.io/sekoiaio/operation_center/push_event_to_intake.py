da4acba16f9c3c91ccf62bc72a3a0baf0224cfcc
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2025-10-28 11:38:56 +0100

    Revert "Sekoia.io - improve push events action"
    This reverts commit 9a32296ca32241c44db79a465e9ba0209cdcea1f.

--- Sekoia.io/sekoiaio/operation_center/push_event_to_intake.py
+++ Sekoia.io/sekoiaio/operation_center/push_event_to_intake.py
@@ -1,12 +1,8 @@
 import json
 from posixpath import join as urljoin
-from typing import Any, Generator, Sequence
 
 import requests
-from requests import Response
 from sekoia_automation.action import Action
-from sekoia_automation.constants import CHUNK_BYTES_MAX_SIZE, EVENT_BYTES_MAX_SIZE
-from tenacity import Retrying, stop_after_delay, wait_exponential
 
 from sekoiaio.utils import user_agent
 
@@ -26,81 +22,6 @@ class PushEventToIntake(Action):
             filepath = self._data_path.joinpath(event_path)
             if filepath.is_file():
                 filepath.unlink()
-
-    def _retry(self):
-        return Retrying(
-            stop=stop_after_delay(3600),  # 1 hour without being able to send events
-            wait=wait_exponential(multiplier=1, min=1, max=10),
-            reraise=True,
-        )
-
-    def _chunk_events(self, events: Sequence) -> Generator[list[Any], None, None]:
-        """
-        Group events by chunk.
-
-        Args:
-            sequence events: Sequence: The events to group
-
-        Returns:
-            Generator[list[Any], None, None]:
-        """
-        chunk: list[Any] = []
-        chunk_bytes: int = 0
-        nb_discarded_events: int = 0
-
-        # iter over the events
-        for event in events:
-            if len(event) > EVENT_BYTES_MAX_SIZE:
-                nb_discarded_events += 1
-                continue
-
-            # if the chunk is full
-            if chunk_bytes + len(event) > CHUNK_BYTES_MAX_SIZE:
-                # yield the current chunk and create a new one
-                yield chunk
-                chunk = []
-                chunk_bytes = 0
-
-            # add the event to the current chunk
-            chunk.append(event)
-            chunk_bytes += len(event)
-
-        # if the last chunk is not empty
-        if len(chunk) > 0:
-            # yield the last chunk
-            yield chunk
-
-        # if events were discarded, log it
-        if nb_discarded_events > 0:
-            self.log(message=f"{nb_discarded_events} too long events " "were discarded (length > 250kb)")
-
-    def _send_chunk(
-        self,
-        intake_key: str,
-        batch_api: str,
-        chunk_index: int,
-        chunk: list[Any],
-        collect_ids: dict[int, list[str]],
-    ):
-        try:
-            request_body = {
-                "intake_key": intake_key,
-                "jsons": chunk,
-            }
-
-            for attempt in self._retry():
-                with attempt:
-                    res: Response = requests.post(
-                        batch_api, json=request_body, timeout=30, headers={"User-Agent": user_agent()}
-                    )
-                    res.raise_for_status()
-
-            collect_ids[chunk_index] = res.json().get("event_ids", [])
-
-        except Exception as ex:
-            message = f"Failed to forward {len(chunk)} events"
-            self.log(message=message, level="error")
-            self.log_exception(ex, message=message)
 
     def run(self, arguments) -> dict:
         events = []
@@ -124,20 +45,17 @@ class PushEventToIntake(Action):
             self.log("No event to push", level="info")
             return {"event_ids": []}
 
-        intake_server = arguments.get("intake_server", "https://intake.sekoia.io")
-        batch_api = urljoin(intake_server, "batch")
-        intake_key = arguments["intake_key"]
+        # pushing the events
+        intake_host = arguments.get("intake_server", "https://intake.sekoia.io")
+        batch_api = urljoin(intake_host, "batch")
+        content = {"intake_key": arguments["intake_key"], "jsons": events}
+        res = requests.post(batch_api, json=content, headers={"User-Agent": user_agent()})
 
-        # Dict to collect event_ids for the API
-        collect_ids: dict[int, list] = {}
-
-        chunks = self._chunk_events(events)
-        for chunk_index, chunk in enumerate(chunks):
-            self._send_chunk(intake_key, batch_api, chunk_index, chunk, collect_ids)
-
-        event_ids = [event_id for chunk_index in sorted(collect_ids.keys()) for event_id in collect_ids[chunk_index]]
+        if res.status_code > 299:
+            self.log(f"Intake rejected events: {res.text}", level="error")
+            res.raise_for_status()
 
         if not arguments.get("keep_file_after_push", False):
             self._delete_file(arguments)
 
-        return {"event_ids": event_ids}
+        return res.json()
