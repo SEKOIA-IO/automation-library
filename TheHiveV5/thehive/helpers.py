07298fed36ae660bdb23d79e1dd711e39b3847ce
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2025-12-19 12:09:32 +0100

    fix(TheHiveV5): apply suggestion from review

--- TheHiveV5/thehive/helpers.py
+++ TheHiveV5/thehive/helpers.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from contextlib import contextmanager
+import shutil
 
 
 @contextmanager
-def copy_to_tempfile(source_path: Path):
+def copy_to_tempfile(source_path: Path) -> str:
     """Context manager to copy a file to a temporary file."""
     with TemporaryDirectory() as temp_dir:
         temp_file_path = Path(temp_dir) / source_path.name
@@ -12,6 +13,6 @@ def copy_to_tempfile(source_path: Path):
             temp_file_path.open("wb") as temp_file,
             source_path.open("rb") as original_file,
         ):
-            temp_file.write(original_file.read())
+            shutil.copyfileobj(original_file, temp_file)
             temp_file.flush()
-            yield temp_file.name
+        yield str(temp_file_path)
