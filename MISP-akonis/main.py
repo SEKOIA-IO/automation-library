5ba749467e9eedb4bddca845bbee27fec0bdf4bf
Imothep-Akonis <cibrahim@akonis.fr>
2025-12-30 13:46:09 +0100

    Removed original trigger from main & changed uuids

--- MISP-akonis/main.py
+++ MISP-akonis/main.py
@@ -1,13 +1,11 @@
 from misp.misp_to_stix import MISPToSTIXAction
 from misp.publish_to_misp import PublishToMISPAction
-from misp.trigger import MISPTrigger
 from sekoia_automation.module import Module
 from misp.trigger_misp_ids_attributes_to_ioc_collection import MISPIDSAttributesToIOCCollectionTrigger
 
 if __name__ == "__main__":
     module = Module()
 
-    module.register(MISPTrigger, "trigger")
     module.register(MISPToSTIXAction, "convert-misp-to-stix")
     module.register(PublishToMISPAction, "publish-to-misp")
 
