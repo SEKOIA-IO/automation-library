625973895357adcd74b125eb5e033e9d58520965
Imothep-Akonis <cibrahim@akonis.fr>
2026-01-13 11:27:37 +0100

    Removed MISP-akonis/

--- MISP-akonis/misp/misp_to_stix.py
+++
@@ -1,9 +0,0 @@
-from misp.misp_to_stix_converter import STIXConverter
-from sekoia_automation.action import Action
-
-
-class MISPToSTIXAction(Action):
-    def run(self, arguments):
-        converter = STIXConverter()
-
-        return {"bundle": converter.convert(arguments["event"])}
