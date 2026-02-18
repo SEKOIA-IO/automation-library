f959c61b7d5363218c62d42a29eaeba3d9a21f47
mchupeau-sk <mehidine.chupeau@sekoia.io>
2026-02-18 09:57:39 +0000

    fix black + update version

--- TEST_MEHIDINE/main.py
+++ TEST_MEHIDINE/main.py
@@ -2,7 +2,6 @@ from test_mehidine_modules import Test_MehidineModule
 
 from test_mehidine_modules.action_request import Request
 
-
 if __name__ == "__main__":
     module = Test_MehidineModule()
     module.register(Request, "Request")
--- TEST_MEHIDINE/test_mehidine_modules/action_request.py
+++ TEST_MEHIDINE/test_mehidine_modules/action_request.py
@@ -2,31 +2,30 @@ from typing import Literal
 
 from pydantic.v1 import BaseModel, HttpUrl
 import requests
-from sekoia_automation.action import Action  
+from sekoia_automation.action import Action
 
 
-class RequestArguments(BaseModel):  
+class RequestArguments(BaseModel):
     url: HttpUrl
     headers: dict | None = None
     method: Literal["get", "post", "put", "patch", "delete"]
 
-class Response(BaseModel):  
-  status_code: int
-  headers: dict
-  text: str
+
+class Response(BaseModel):
+    status_code: int
+    headers: dict
+    text: str
 
 
-class Request(Action):  
+class Request(Action):
     """
     Action to request an HTTP resource
     """
 
     results_model = Response
 
-    def run(self, arguments: RequestArguments) -> Response:  
-        self.log(  
-          message=f"Request URL module started. Target URL: {arguments.url}", level="info"
-          )
+    def run(self, arguments: RequestArguments) -> Response:
+        self.log(message=f"Request URL module started. Target URL: {arguments.url}", level="info")
 
         response = requests.request(
             method=arguments.method,
@@ -36,12 +35,10 @@ class Request(Action):
 
         if not response.ok:
             # Will end action as in error
-            self.error(  
-              f"HTTP Request failed: {arguments.url} with {response.status_code}"
-              )
+            self.error(f"HTTP Request failed: {arguments.url} with {response.status_code}")
 
-        return Response(  
-          status_code=response.status_code,
-          headers=dict(response.headers),
-          text=response.text,
+        return Response(
+            status_code=response.status_code,
+            headers=dict(response.headers),
+            text=response.text,
         )
