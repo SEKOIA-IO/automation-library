1a6c1f5f7d3db63ef550616a64a37411e98d2ab3
mchupeau-sk <mehidine.chupeau@sekoia.io>
2026-05-18 11:46:39 +0000

    fix pipeline

--- MicrosoftDefender/microsoftdefender_modules/models.py
+++ MicrosoftDefender/microsoftdefender_modules/models.py
@@ -4,5 +4,5 @@ from pydantic import BaseModel, Field
 class MicrosoftDefenderModuleConfiguration(BaseModel):
     base_url: str = Field("https://api.securitycenter.microsoft.com", description="Base URL")
     app_id: str = Field(..., description="App ID")
-    app_secret: str = Field(..., description="App Secret", secret=True)
+    app_secret: str = Field(..., description="App Secret", json_schema_extra={"secret": True})
     tenant_id: str = Field(..., description="Tenant ID")
