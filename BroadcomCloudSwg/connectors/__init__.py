d590e79b9474d53761c9ace82226bbe6b4cc8dda
TOUFIKI Zakarya <zakarya.toufiki@sekoia.io>
2026-05-26 10:40:03 +0200

    fix(broadcom): fix pydantic

--- BroadcomCloudSwg/connectors/__init__.py
+++ BroadcomCloudSwg/connectors/__init__.py
@@ -1,6 +1,6 @@
 """Package with connectors to work with Broadcom Cloud SWG."""
 
-from pydantic import BaseModel, Field
+from pydantic.v1 import BaseModel, Field
 from sekoia_automation.module import Module
 
 
@@ -8,8 +8,8 @@ class BroadcomCloudModuleConfig(BaseModel):
     """Configuration for BroadcomCloudModuleConfig."""
 
     ratelimit_per_minute: int = 60
-    username: str = Field(..., description="Broadcom Cloud Username")
-    password: str = Field(..., description="Broadcom Cloud Password")
+    username: str = Field(required=True, description="Broadcom Cloud Username")
+    password: str = Field(required=True, description="Broadcom Cloud Password")
 
 
 class BroadcomCloudModule(Module):
