2884872d85ad64dd14a1830d6ba6c9f636419341
mchupeau-sk <218948903+mchupeau-sk@users.noreply.github.com>
2026-08-04 07:36:37 +0000

    Upstream changes into production branch

--- Zimperium/zimperium_modules/client/__init__.py
+++ Zimperium/zimperium_modules/client/__init__.py
@@ -15,9 +15,7 @@ class ApiClient(requests.Session):
         ratelimit_per_minute: int = 600,
     ):
         super().__init__()
-        self.auth = ZimperiumApiAuthentication(
-            base_url=base_url, client_id=client_id, client_secret=client_secret
-        )
+        self.auth = ZimperiumApiAuthentication(base_url=base_url, client_id=client_id, client_secret=client_secret)
         self.mount(
             "https://",
             LimiterAdapter(
