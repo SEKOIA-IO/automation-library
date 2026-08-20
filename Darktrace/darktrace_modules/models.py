1e539a65ffd084e8c4a7fbad919a1bc3ab8598d0
Thomas Fanouillere <thomas.fanouillere@sekoia.io>
2026-08-20 12:57:33 +0000

    allow on-prem setup without cloud api credentials

--- Darktrace/darktrace_modules/models.py
+++ Darktrace/darktrace_modules/models.py
@@ -2,10 +2,10 @@ from pydantic import BaseModel, Field
 
 
 class DarktraceModuleConfiguration(BaseModel):
-    api_url: str = Field(..., description="The url of the Darktrace appliance")
-    public_key: str = Field(..., description="The public key to the Darktrace API")
-    private_key: str = Field(
-        ...,
+    api_url: str | None = Field(default=None, description="The url of the Darktrace appliance")
+    public_key: str | None = Field(default=None, description="The public key to the Darktrace API")
+    private_key: str | None = Field(
+        default=None,
         description="The private key to the Darktrace API",
         json_schema_extra={"secret": True},
     )
