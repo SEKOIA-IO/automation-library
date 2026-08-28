4923886ceba6c2415d93be3680420ec7cfd5222c
vg-svitla <v.huriev@svitla.com>
2026-08-28 18:40:35 +0400

    Fix: SentinelOne bump sdk version

--- SentinelOne/tests/test_filters.py
+++ SentinelOne/tests/test_filters.py
@@ -1,3 +1,4 @@
+from typing import ClassVar
 from management.common.query_filter import QueryFilter
 from management.mgmtsdk_v2.services.agent_actions import AgentsDangerousActionFilter
 
@@ -7,14 +8,13 @@ from sentinelone_module.filters import BaseFilters
 class TestArguments(BaseFilters):
     __test__ = False
 
-    account_ids: list[str] | None
-    group_ids: list[str] | None
-    ids: list[str] | None
-    site_ids: list[str] | None
-    query: str | None
+    account_ids: list[str] | None = None
+    group_ids: list[str] | None = None
+    ids: list[str] | None = None
+    site_ids: list[str] | None = None
+    query: str | None = None
 
-    class Config:
-        query_filter_class = AgentsDangerousActionFilter
+    query_filter_class: ClassVar[type[QueryFilter]] = AgentsDangerousActionFilter
 
 
 def test_to_filters():
